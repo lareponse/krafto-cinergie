ddabd45d5026adf001f7434e7eb09fd58fcd2654
HexMakina <20891495+HexMakina@users.noreply.github.com>
2023-05-10 16:45:42 +0200

    improved dropdown for past decades

--- app/Views/Secret/_partials/filters/FiltersOnYearAndMonth.php
+++ app/Views/Secret/_partials/filters/FiltersOnYearAndMonth.php
@@ -1,31 +1,50 @@
-<nav class="mt-5" aria-label="filter by year">
+<div class="mt-4 text-left">
+    <?php
+    $firstYear ??= 1989;
+    $lastYear ??= intval(date('Y'));
+
+    $lastDecade =  intval($lastYear / 10) * 10;
+    $firstDecade =  intval($firstYear / 10) * 10;
+
+    foreach (range($lastYear, $lastDecade, 1) as $year) {
+        $href = $controller->url('list', ['year' => $year]);
+        $class = $filters['year'] == $year ? 'btn-primary' : 'btn-outline-primary';
+        echo '<a href="' . $href . '" class="btn btn mx-1 ' . $class . '">' . $year . '</a>';
+    }
+
+    foreach (range($lastDecade, $firstDecade, 10) as $decade) {
+        $startYear = $decade - 10;
+
+        $class = 'btn-outline-primary';
+        $label = $startYear;
+
+        if ($filters['year'] >= $startYear && $filters['year'] < $decade) {
+            $class = 'btn-primary';
+            $label = $filters['year'];
+        }
+
+        echo '<a type="button" class="btn ' . $class . ' dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">' . $label . '</a>';
+        echo '<ul class="dropdown-menu">';
+        foreach (range($decade - 1, $startYear, 1) as $year) {
+            $href = $controller->url('list', ['year' => $year]);
+            $class = $filters['year'] == $year ? 'bg-primary text-light fw-bold' : '';
+            echo '<li><a class="dropdown-item justify-content-center ' . $class . '" href="' . $href . '">' . $year . '</a></li>';
+        }
+        echo '</ul>';
+    }
+
+    ?>
+</div>
+
+
+<div class="mt-3 text-left" role="group" aria-label="Button group with nested dropdown">
     <?php
 
-    $lastYear ??= intval(date('Y'));
-    $firstYear ??= $lastYear-10;
-
-    $filters['year'] ??= $lastYear;
-    $filters['month'] ??= intval(date('m'));
-
-    $link_template = '<a href="%s" class="btn btn-sm %s m-1 p-1 px-2">%s</a>';
-    foreach (array_reverse(range($firstYear, $lastYear)) as $year) {
-        $href = $controller->url('list', ['year' => $year]);
-
-        $class = $filters['year'] == $year ? 'btn-secondary' : 'btn-outline-secondary';
-
-        echo sprintf($link_template, $href, $class, $year);
+    foreach (range(1, 12) as $month) {
+        $href = $controller->url('list', ['year' => $filters['year'], 'month' => $month]);
+        $class = $filters['month'] == $month ? 'btn-secondary' : 'btn-outline-secondary';
+        echo '<a href="' . $href . '" class="btn btn-sm mx-1 ' . $class . '">' . $month . '</a>';
     }
 
-    if (isset($filters['year'])) {
-        echo '</nav><nav class="mt-1" aria-label="filter by month">';
-
-        foreach (range(1, 12) as $month) {
-            $href = $controller->url('list', ['year' => $filters['year'], 'month' => $month]);
-
-            $class = $filters['month'] == $month ? 'btn-secondary' : 'btn-outline-secondary';
-
-            echo sprintf($link_template, $href, $class, $month);
-        }
-    }
     ?>
-</nav>
+</div>
