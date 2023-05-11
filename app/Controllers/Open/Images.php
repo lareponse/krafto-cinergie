3b6dd4f1098e5739c7eedc62692bbb4a39e8a31c
HexMakina <20891495+HexMakina@users.noreply.github.com>
2023-05-11 16:05:51 +0200

    improved legacy handler

--- app/Controllers/Open/Images.php
+++ app/Controllers/Open/Images.php
@@ -4,24 +4,34 @@ namespace App\Controllers\Open;
 
 class Images extends Home
 {
-  public function legacy()
-  {
-    $path = $this->router()->params('path');
-    $parts = pathinfo($path);
-
-    $newFileNames = array_unique([$parts['filename'], str_replace('_', '-', $parts['filename'])]);
-    foreach ($newFileNames as $filename) {
-      
-      $newPath = $parts['dirname'] . '/' . $filename . '.' . $parts['extension'];
-
-      if (file_exists($this->get('settings.folders.images') . '/' . $newPath)) {
-        header("HTTP/1.1 301 Moved Permanently");
-        header('Location: '.$this->get('settings.urls.images') . '/' . $newPath);
+    public function legacy()
+    {
+        $path = $this->router()->params('path');
+        $parts = pathinfo($path);
+        foreach($this->possibleFileNames($parts) as $filename) {
+            
+            $relativePath = $parts['dirname'] . '/' . $filename . '.' . $parts['extension'];
+            $absolutePath = $this->get('settings.folders.images') . '/' . $relativePath;
+            
+            if (file_exists($absolutePath)) {
+                header("HTTP/1.1 301 Moved Permanently");
+                header('Location: ' . $this->get('settings.urls.images') . '/' . $relativePath);
+                exit;
+            }
+        }
+        header("HTTP/1.1 404 Moved Permanently");
         exit;
-      }
     }
 
-    header("HTTP/1.1 404 Moved Permanently");
-    exit;
-  }
+    private function subPath()
+    {
+
+    }
+    private function possibleFileNames($parts)
+    {
+        return array_unique([
+                $parts['filename'], 
+                str_replace('_', '-', $parts['filename'])
+            ]);
+    }
 }
