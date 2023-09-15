b1340e3ed1c2df23a8365c3b0e31c71f5be87262
HexMakina <20891495+HexMakina@users.noreply.github.com>
2023-09-15 02:46:44 +0200

    added Event VC + locale js

--- app/Controllers/Open/Event.php
+++ app/Controllers/Open/Event.php
@@ -2,7 +2,22 @@
 
 namespace App\Controllers\Open;
 
+use DateTimeImmutable;
+use DateTimeZone;
+
 class Event extends Kortex
 {
 
+    public function agenda()
+    {
+        $current = new \DateTimeImmutable();
+        // $date = new \DateTimeImmutable();
+        // $date->setDate($current->format('Y'), $current->format('m'), 1);
+        $formatter = new \IntlDateFormatter('fr_FR', \IntlDateFormatter::NONE, \IntlDateFormatter::NONE, null, null, 'MMMM');
+        // Format the date
+        $month = $formatter->format($current);
+        $this->viewport('current', $current);
+        $this->viewport('current_month', ucfirst($month));
+        $this->viewport('current_year', $current->format('Y'));
+    }
 }
