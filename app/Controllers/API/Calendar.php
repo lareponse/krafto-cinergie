a2ba6f81c542ac63bc52786a54a1da9e14bef477
HexMakina <20891495+HexMakina@users.noreply.github.com>
2023-09-18 21:57:42 +0200

    added REST events

--- app/Controllers/API/Calendar.php
+++ app/Controllers/API/Calendar.php
@@ -33,5 +33,34 @@ class Calendar extends \HexMakina\kadro\Controllers\Kadro
 
     }
 
+    public function events()
+    {
+
+        $res = Event::filter(['date_start' => $this->router()->params('start'), 'date_stop' => $this->router()->params('end')]);
+        // dd($res);
+        $events = [];
+        foreach($res as $event){
+            $events[]= [
+                'categorie' => 'evt-'.$event->get('type_reference'),
+                'className' => 'evt-'.$event->get('type_reference'),
+                'title' => $event->__toString(),
+                'start' => $event->get('starts'),
+                'end' => $event->get('ends'),
+                'url_site' => $event->get('url_site'),
+                'url_internal' => $event->get('url_internal')
+            ];
+        }
+        echo(json_encode($events));
+        die;
+    }
+/*
+{
+          categorie: 'categorie2',
+          title: 'Business Lunch',
+          start: '2023-01-03T13:00:00',
+          constraint: 'businessHours',
+          color: '#ea9909'
+        }
+        */
 
 }
