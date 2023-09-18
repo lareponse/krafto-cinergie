aecc0811788cde31ff8f51797b942d09bf8a22ba
HexMakina <20891495+HexMakina@users.noreply.github.com>
2023-09-18 14:36:15 +0200

    homepage: early draft, missing Work component

--- app/Controllers/Open/Home.php
+++ app/Controllers/Open/Home.php
@@ -2,7 +2,41 @@
 
 namespace App\Controllers\Open;
 
+use App\Models\{Article, Contest, Event};
+
 class Home extends Kortex
 {
 
+    protected $pageSlug = 'home';
+        
+    public function home()
+    {
+        $query = $this->get('Controllers\\Open\\Article')->queryListing();
+        $query->whereEQ('isDiaporama', '1');
+        $this->viewport('articlesDiaporama', $query->retObj(Article::class));
+        
+        $query = $this->get('Controllers\\Open\\Article')->queryListing();
+        $query->whereEQ('isDiaporama', '0');
+        $query->whereEQ('type_id', '50');
+        $query->whereNotEmpty('embedVideo');
+        $query->limit(3);
+        $this->viewport('entrevuesFilmees', $query->retObj(Article::class));
+
+        $query = $this->get('Controllers\\Open\\Article')->queryListing();
+        $query->whereEQ('isDiaporama', '0');
+        $query->whereEmpty('embedVideo');
+        $query->limit(3);
+        $this->viewport('sousLaLoupe', $query->retObj(Article::class));
+
+        $contests = Contest::filter(['active' => '1', ['order_by' => [['contest', 'starts', 'ASC']]]]);
+        $this->viewport('contests', $contests);
+
+        $events = Event::filter(['active' => '1'], ['limit' => '3', 'order_by' => [['event', 'starts', 'ASC']]]);
+        $this->viewport('events', $events);
+
+        $works = [];
+        $this->viewport('works', $works);
+
+
+    }
 }
