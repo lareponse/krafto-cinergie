bdca27cea36dc75be61e5c269143c425026293a8
HexMakina <20891495+HexMakina@users.noreply.github.com>
2023-09-24 01:48:50 +0200

    added praxisIds (exploded)

--- app/Models/Abilities/HasPraxis.php
+++ app/Models/Abilities/HasPraxis.php
@@ -10,10 +10,16 @@ trait HasPraxis
     public function praxis() : array
     {
         if(empty($this->praxis)){
-            $this->praxis = $this->tags(explode(',',$this->get('praxis_ids')));
+            $this->praxis = $this->tags($this->praxisIds());
         }
 
         return $this->praxis;
+    }
+    
+    public function praxisIds(): array
+    {
+        if(!empty($this->get('praxis_ids')))
+            return explode(',',$this->get('praxis_ids'));
     }
 
 }
