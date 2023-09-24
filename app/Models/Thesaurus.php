9b9cd62c02245026ff67f844dc1b5c6d7e6fa9f8
HexMakina <20891495+HexMakina@users.noreply.github.com>
2023-09-24 02:09:54 +0200

    Thesarus is activable and filterable on first char

--- app/Models/Thesaurus.php
+++ app/Models/Thesaurus.php
@@ -8,9 +8,25 @@ use HexMakina\TightORM\TightModel;
 class Thesaurus extends TightModel
 {
     use Abilities\HasSlug;
+    use Abilities\IsActivable;
+
+    use Abilities\FiltersOnFirstChar;
+
 
     public function __toString()
     {
         return $this->get('label');
+    }
+
+    public static function query_retrieve($filters = [], $options = []): SelectInterface
+    {
+        $Query = parent::query_retrieve($filters, $options);
+
+        if(isset($filters['FiltersOnFirstChar'])){
+            self::applyFirstCharFilter($filters['FiltersOnFirstChar'], $Query, 'label');
+        }
+
+        $Query->orderBy(['label', 'ASC']);
+        return $Query;
     }
 }
