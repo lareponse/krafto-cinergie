768db9a77b2f5f17341478cf4b403c0ebe6dee97
HexMakina <20891495+HexMakina@users.noreply.github.com>
2023-09-24 23:20:30 +0200

    added controls to set/unset profile picture

--- app/Views/Secret/_partials/tab-images.php
+++ app/Views/Secret/_partials/tab-images.php
@@ -21,18 +21,32 @@
             <div class="card border-0">
   
                 <div href="<?= '@' ?>" class="card-body card-image-background  cursor-move" style="background-image:url('<?= $path; ?>');">
- 
                 </div>
+
                 <div class="controls">
-
-                    <?php $route = $controller->router()->hyp('relation_image_unlink', ['controller' => $controller->className(), 'id' => $controller->loadModel()->getID()]); ?>
-
+                    <?php 
+                    $route = $controller->router()->hyp('relation_image_unlink', ['controller' => $controller->className(), 'id' => $controller->loadModel()->getID()]); 
+                    ?>
                     <form action="<?= $route ?>" method="POST" class="control">
                         <input type="hidden" name="filename" value="<?= $image?>" />
-                        <button type="submit" href="" class="icon text-primary p-1"><?= $this->icon('delete', 14);?></button>
+                        <button type="submit" href="#" class="icon text-primary p-1"><?= $this->icon('delete', 18);?></button>
                     </form>
 
-                    <a class="control text-secondary p-1"><?= $this->icon('eye', 14);?></a>
+                    <?php
+                    if($controller->loadModel()->hasProfilePicture() && $controller->loadModel()->profilePicturePath() === $path){
+                        $class = 'text-success';
+                        $href = $controller->router()->hyp('dash_record_unset_profile_picture', ['controller' => $controller->className(), 'id' => $controller->loadModel()->getID()]);
+                        $title = 'Retirer comme photo principale';
+                    }
+                    else{
+                        $class ='text-secondary';
+                        $href = $controller->router()->hyp('dash_record_set_profile_picture', ['controller' => $controller->className(), 'id' => $controller->loadModel()->getID(), 'path' => $path]);
+                        $title = 'Définir comme photo principale';
+
+                    }
+                    ?>
+                    <a href="<?= $href?>" class="control <?= $class ?> p-1"><?= $this->icon('profilePicture', 18, ['title' => $title]);?></a>
+                    <a href="#" class="control text-secondary p-1"><?= $this->icon('eye', 18);?></a>
                 </div>
             </div>
         </div>
