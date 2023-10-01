7a8e689bd78c0540320c1f5c776cc696b17503ee
HexMakina <20891495+HexMakina@users.noreply.github.com>
2023-10-01 22:53:51 +0200

    improved: conditional rendering of legacy data

--- app/Views/Secret/_partials/legacy_view.php
+++ app/Views/Secret/_partials/legacy_view.php
@@ -1,17 +1,25 @@
-<h3 class="h6 small text-secondary text-uppercase mb-3">Legacy</h3>
-
-<ul class="list-unstyled text-muted">
 <?php
 $columns = get_class($model)::table()->columns();
+$legacyValues = [];
 foreach ($columns as $col) {
     if (strpos($col->name(),'legacy_') === 0 ) {
         $value = trim($controller->formModel()->get($col->name()));
         $icon = $this->icon('info', 18, ['class' => 'me-2', 'title' => $col->name(), 'alt' => $col->name()]);
         if('' !== $value) {
-            echo $this->DOM()::li($icon.$value, ['class' => 'py-2']);
+            $legacyValues[] = $this->DOM()::li($icon.$value, ['class' => 'py-2']);
         }
     }
 }
-
+if(!empty($legacyValues)){
+    ?>
+<div class="card border-0 pt-3">
+    <div class="card-body pt-0">
+    <h3 class="h6 small text-secondary text-uppercase mb-3">Legacy</h3>
+    <ul class="list-unstyled text-muted">
+        <?= implode('', $legacyValues)?>
+    </ul>
+    </div>
+</div>
+<?php
+}
 ?>
-</ul>
