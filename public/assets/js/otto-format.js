93ef44a044d7740408d562b102d32be81cad39b4
HexMakina <20891495+HexMakina@users.noreply.github.com>
2023-10-17 23:48:32 +0200

    clean up

--- public/assets/js/otto-format.js
+++ public/assets/js/otto-format.js
@@ -2,23 +2,23 @@ document.addEventListener("DOMContentLoaded", function () {
 
     document.querySelectorAll('.otto-date').forEach(container => {
 
-        let date = container.innerText.trim();
-        if (date.length > 0) {
+        let dateText = container.innerText.trim();
+        if (dateText.length > 0) {
+            
             let format = { dateStyle: "long" } // default
+            
             try {
-                date = new Date(date)
-
+                date = new Date(dateText)
                 if (container.getAttribute('otto-format')) {
                     let try_format = decodeURIComponent(container.getAttribute('otto-format'))
+                    
                     try_format = JSON.parse(try_format);
                     format = try_format;
-                    console.error("Error parsing JSON:", error);
                 }
-
                 container.innerText = new Intl.DateTimeFormat('fr-FR', format).format(date)
             }
-            catch (error) {
-                console.error(error.message, ':', date)
+            catch (exception) {
+                console.error(dateText, exception, date);
             }
         }
     })
