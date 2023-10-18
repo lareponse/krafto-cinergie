fdb28da5c4909184a2053b92c66225469f7004bf
HexMakina <20891495+HexMakina@users.noreply.github.com>
2023-10-18 16:32:55 +0200

    improved behavior with non empty, illegal date string

--- public/assets/js/otto-format.js
+++ public/assets/js/otto-format.js
@@ -3,23 +3,29 @@ document.addEventListener("DOMContentLoaded", function () {
     document.querySelectorAll('.otto-date').forEach(container => {
 
         let dateText = container.innerText.trim();
-        if (dateText.length > 0) {
-            
-            let format = { dateStyle: "long" } // default
-            
-            try {
-                date = new Date(dateText)
-                if (container.getAttribute('otto-format')) {
-                    let try_format = decodeURIComponent(container.getAttribute('otto-format'))
-                    
-                    try_format = JSON.parse(try_format);
-                    format = try_format;
-                }
-                container.innerText = new Intl.DateTimeFormat('fr-FR', format).format(date)
+        if (dateText.length == 0) {
+            return;
+        }
+
+        let date = new Date(dateText);
+        if (isNaN(date)) {
+            return; // skip this one
+        }
+
+        let format = { dateStyle: "long" } // default
+
+        try {
+            if (container.getAttribute('otto-format')) {
+                let try_format = decodeURIComponent(container.getAttribute('otto-format'))
+                try_format = JSON.parse(try_format);
+                format = try_format;
             }
-            catch (exception) {
-                console.error(dateText, exception, date);
-            }
+            
+            container.innerText = new Intl.DateTimeFormat('fr-FR', format).format(date)
+        }
+        catch (exception) {
+            console.log(exception);
+            return; // exit the program
         }
     })
 })
