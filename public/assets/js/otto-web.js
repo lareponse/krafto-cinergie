62be1b7aad11735f0b2c07f4f07a8a29473fa331
HexMakina <20891495+HexMakina@users.noreply.github.com>
2023-10-26 22:30:08 +0200

    refactored as OO and removed codec functions

--- public/assets/js/otto-web.js
+++ public/assets/js/otto-web.js
@@ -1,111 +1,69 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Function to format URLs with class 'otto-url'
-    function formatUrls() {
+class OttoWeb {
+    constructor() {
+        this.urlLinks();
+        this.emailLinks();
+        this.callLinks();
+    }
+
+    urlLinks() {
         document.querySelectorAll('.otto-url').forEach(container => {
             const anchorElement = document.createElement('a');
             const url = container.innerHTML.trim();
 
-            // Check if the URL is valid
             if (/^(http:\/\/|https:\/\/)[\w\.-]+\.\w+$/i.test(url)) {
                 anchorElement.href = url;
                 anchorElement.textContent = url;
             } else {
-                anchorElement.href = 'https://' + url; // Assuming it's a valid domain, add https://
+                anchorElement.href = 'https://' + url;
                 anchorElement.textContent = url;
             }
 
-            container.innerHTML = ''; // Clear container's innerHTML
+            container.innerHTML = '';
             container.append(anchorElement);
         });
     }
 
-    // Function to format email addresses with class 'otto-email'
-    function formatEmails() {
+    emailLinks() {
         document.querySelectorAll('.otto-email').forEach(container => {
-            const email = container.innerHTML.trim();
-            const emailElement = document.createElement('a');
-
-            // Encode the email address
-            const encodedEmail = encodeEmail(email);
-
-            // Get the subject and content from attributes
-            const subject = container.getAttribute('otto-subject') || ''; // Default subject
-            const content = container.getAttribute('otto-content') || ' Content'; // Default content
-
-            // Replace \n with <br> for HTML line breaks in the email content
-            const formattedContent = content.replace(/\n/g, '<br>');
-
-            // Set the obfuscated mailto link with subject and content
-            emailElement.href = 'javascript:void(0);';
+            console.log(container)
+            const email = container.innerHTML.trim()
+            const emailElement = document.createElement('a')
+            
+            emailElement.textContent = email
+            emailElement.href = 'javascript:void(0);'
             emailElement.addEventListener('click', function () {
-                const emailSubject = encodeURIComponent(subject);
-                const emailContent = encodeURIComponent(formattedContent);
-                window.location.href = `mailto:${decodeEmail(encodedEmail)}?subject=${emailSubject}&body=${emailContent}`;
+                let emailSubject = encodeURIComponent(container.getAttribute('otto-subject') || '')
+
+                let emailContent = container.getAttribute('otto-content') || ' Content'
+                emailContent = encodeURIComponent(emailContent.replace(/\n/g, '<br>'))
+
+                window.location.href = `mailto:${email}?subject=${emailSubject}&body=${emailContent}`
             });
 
-            emailElement.textContent = decodeEmail(encodedEmail);
-            container.innerHTML = '';
-            container.append(emailElement);
+            container.innerHTML = ''
+            container.append(emailElement)
         });
     }
 
-    // Function to encode and decode email addresses
-    function encodeEmail(email) {
-        const encodedEmail = [];
-        for (let i = 0; i < email.length; i++) {
-            encodedEmail.push(String.fromCharCode(email.charCodeAt(i) + 1));
-        }
-        return encodedEmail.join('');
-    }
-
-    function decodeEmail(encodedEmail) {
-        const decodedEmail = [];
-        for (let i = 0; i < encodedEmail.length; i++) {
-            decodedEmail.push(String.fromCharCode(encodedEmail.charCodeAt(i) - 1));
-        }
-        return decodedEmail.join('');
-    }
-
-    // Function to format phone numbers with class 'otto-phone'
-    function formatPhoneNumbers() {
+    callLinks() {
         document.querySelectorAll('.otto-phone').forEach(container => {
             const phoneNumber = container.innerHTML.trim();
-            const phoneNumberElement = document.createElement('a');
+            const phoneNormalized = phoneNumber.replace(/\s/g, '');
 
-            // Encode the phone number
-            const encodedPhoneNumber = encodePhoneNumber(phoneNumber);
+            const callLink = document.createElement('a');
 
-            // Set the obfuscated tel link
-            phoneNumberElement.href = 'javascript:void(0);';
-            phoneNumberElement.addEventListener('click', function () {
-                window.location.href = 'tel:' + decodePhoneNumber(encodedPhoneNumber);
+            callLink.href = 'javascript:void(0);';
+            callLink.addEventListener('click', function () {
+                window.location.href = 'tel:' + phoneNormalized;
             });
 
-            phoneNumberElement.textContent = decodePhoneNumber(encodedPhoneNumber);
+            callLink.textContent = phoneNumber;
             container.innerHTML = '';
-            container.append(phoneNumberElement);
+            container.append(callLink);
         });
     }
+}
 
-    // Function to encode and decode phone numbers
-    function encodePhoneNumber(phoneNumber) {
-        const encodedPhoneNumber = [];
-        for (let i = 0; i < phoneNumber.length; i++) {
-            encodedPhoneNumber.push(String.fromCharCode(phoneNumber.charCodeAt(i) + 1));
-        }
-        return encodedPhoneNumber.join('');
-    }
-
-    function decodePhoneNumber(encodedPhoneNumber) {
-        const decodedPhoneNumber = [];
-        for (let i = 0; i < encodedPhoneNumber.length; i++) {
-            decodedPhoneNumber.push(String.fromCharCode(encodedPhoneNumber.charCodeAt(i) - 1));
-        }
-        return decodedPhoneNumber.join('');
-    }
-
-    // Call the formatting functions
-    formatUrls();
-    formatEmails();
-    formatPhoneNumbers();
+document.addEventListener("DOMContentLoaded", function () {
+    new OttoWeb();
 });
