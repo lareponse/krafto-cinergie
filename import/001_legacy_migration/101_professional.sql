7f3aa91a32280852c3f16a0a26e5f8979bcb3261
HexMakina <20891495+HexMakina@users.noreply.github.com>
2023-11-06 14:42:21 +0100

    professional is business table

--- import/001_legacy_migration/101_professional.sql
+++ import/001_legacy_migration/101_professional.sql
@@ -1,14 +1,24 @@
   DROP TABLE IF EXISTS `cinergie`.`professional`;
 
   CREATE TABLE `cinergie`.`professional` (
-    `id` int NOT NULL,
+    `id` int NOT NULL AUTO_INCREMENT,
+    `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     `slug` varchar(222) NOT NULL,
 
-    `lastname` varchar(30) NOT NULL COMMENT 'leg:nom',
-    `firstname` varchar(30) DEFAULT NULL,
-
+    `label` varchar(255) NOT NULL COMMENT 'concat lastname and firstname',
+    `rank` smallint UNSIGNED DEFAULT NULL COMMENT 'leg:tri',
+    
+    `avatar` varchar(255) DEFAULT NULL COMMENT 'leg:legacy_photo',
     `content` text DEFAULT NULL COMMENT 'leg:presentation',
+
+    `public` tinyint(1) NOT NULL DEFAULT '0' COMMENT '0: view in backend only',
+    `pick` tinyint(1) NOT NULL DEFAULT '0' COMMENT '1: picked for home page',
+    `listable` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1: appears in general listings',
+    `searchable` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1: appears in search results',
+
+    `lastname` varchar(30) NOT NULL COMMENT 'leg:nom',
+    `firstname` varchar(30) DEFAULT NULL COMMENT 'leg:prenom',
 
     `filmography` text DEFAULT NULL,
 
@@ -30,39 +40,43 @@
     `city` varchar(40) DEFAULT NULL COMMENT 'leg:ville',
     `street` varchar(100) DEFAULT NULL COMMENT 'leg:adresse',
 
-    `isListed` tinyint(1) DEFAULT NULL COMMENT 'leg:inAnnuaire',
 
     `declic_image` varchar(80) DEFAULT NULL,
     `declic_texte` text,
     `declic_signature` varchar(30) DEFAULT NULL,
 
-    `updated_on` datetime NOT NULL COMMENT 'leg:maj',
+    `leg_maj` datetime NOT NULL COMMENT 'updated on',
 
-    `profilePicture` varchar(255) DEFAULT NULL COMMENT 'leg:legacy_photo'
+
+    PRIMARY KEY (`id`),
+    INDEX(`label`)
 
   ) ENGINE=InnoDB CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 
-  -- PRIMARY
-  ALTER TABLE `cinergie`.`professional` ADD PRIMARY KEY (`id`);
-  ALTER TABLE `cinergie`.`professional` MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  ALTER TABLE `cinergie`.`professional` ADD UNIQUE KEY `professional-unique-slug` (`slug`) USING BTREE;
+
 
   -- INDEX
-  ALTER TABLE `cinergie`.`professional` ADD UNIQUE `professional-slug-unique` (`slug`);
   ALTER TABLE `cinergie`.`professional` ADD INDEX(`lastname`);
   ALTER TABLE `cinergie`.`professional` ADD INDEX(`firstname`);
   ALTER TABLE `cinergie`.`professional` ADD INDEX(`birth`);
-  ALTER TABLE `cinergie`.`professional` ADD INDEX(`isListed`);
   -- DATA
   TRUNCATE `cinergie`.`professional`;
 
   INSERT INTO `cinergie`.`professional` (
     `id`,
+
     `slug`,
+    
+    `label`,
 
-    `lastname`,
-    `firstname`,
-
+    `avatar`,
     `content`,
+
+    `listable`,
+
+    `firstname`,
+    `lastname`,
 
     `filmography`,
 
@@ -83,22 +97,25 @@
     `city`,
     `street`,
 
-    `isListed`,
-
     `declic_image`,
     `declic_texte`,
     `declic_signature`,
 
-    `profilePicture`,
-    `updated_on`
+    `leg_maj`
   )
   SELECT
    `id` as `id`,
-   `urlparms` as `slug`,
 
-   TRIM(`nom`) as `lastname`,
-   TRIM(`prenom`) as `firstname`,
+   `urlparms` as `slug`,
+   CONCAT(TRIM(`prenom`), ' ', TRIM(`nom`)) as `label`,
+
+   TRIM(`photo`) as `avatar`,
    TRIM(`information`) as `content`,
+
+   `inAnnuaire` as `listable`,
+
+   TRIM(`prenom`) as `firstname`,
+   TRIM(`nom`) as `lastname`,
 
    TRIM(`filmographie`) as `filmography`,
 
@@ -113,20 +130,17 @@
    TRIM(`email`),
    TRIM(`confidentiel`) as  `secret`,
 
-
    TRIM(`pays`) as `country`,
    TRIM(`region`) as `province`,
    TRIM(`cp`) as `zip`,
    TRIM(`ville`) as `city`,
    TRIM(`adresse`) as `street`,
 
-   `inAnnuaire` as `isListed`,
 
-   `declic_image`,
-   `declic_texte`,
-   `declic_signature`,
+   TRIM(`declic_image`),
+   TRIM(`declic_texte`),
+   TRIM(`declic_signature`),
 
-   `photo`as `profilePicture`,
-   `maj`as `updated_on`
+   `maj`as `leg_maj`
 
   FROM `a7_cinergie_beta`.`personne`
