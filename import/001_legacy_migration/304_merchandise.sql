f0b63f1418889a21816bedb43000754001a42aed
HexMakina <20891495+HexMakina@users.noreply.github.com>
2023-11-06 22:40:38 +0100

    merchandise is business table

--- import/001_legacy_migration/304_merchandise.sql
+++ import/001_legacy_migration/304_merchandise.sql
@@ -2,38 +2,37 @@
 DROP TABLE IF EXISTS `cinergie`.`merchandise`;
 
 CREATE TABLE `merchandise` (
-  `id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-  `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `isActive` tinyint(1) NOT NULL DEFAULT '0',
   `slug` varchar(222) DEFAULT NULL,
+  
   `rank` smallint UNSIGNED DEFAULT NULL,
-
   `label` varchar(255) DEFAULT NULL,
-  `content` text,
+
+  `avatar` varchar(255) DEFAULT NULL COMMENT 'image filename',
+  `content` text DEFAULT NULL,
+
+  `public` tinyint(1) NOT NULL DEFAULT '0' COMMENT '0: view in backend only',
+  `pick` tinyint(1) NOT NULL DEFAULT '0' COMMENT '1: picked for home page',
+  `listable` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1: appears in general listings',
+  `searchable` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1: appears in search results',
 
   `people` varchar(222) DEFAULT NULL,
   `price` float DEFAULT NULL,
   `deliveryBe` float DEFAULT NULL,
   `deliveryEu` float DEFAULT NULL,
 
-  `profilePicture` varchar(255) DEFAULT NULL,
-
   `isBook` tinyint(1) NOT NULL DEFAULT '0',
 
   `legacy_id` varchar(40) DEFAULT NULL,
-  `legacy_lien` varchar(80) DEFAULT NULL
+  `legacy_lien` varchar(80) DEFAULT NULL,
+
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `merchandise-unique-slug` (`slug`) USING BTREE,
+  INDEX(`label`)
+
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
-
--- PRIMARY
-ALTER TABLE `merchandise` ADD PRIMARY KEY (`id`);
-ALTER TABLE `merchandise` MODIFY `id` int NOT NULL AUTO_INCREMENT;
-
-
--- INDEX
-ALTER TABLE `merchandise` ADD UNIQUE `merchandise-slug-unique` (`slug`);
-ALTER TABLE `merchandise` ADD INDEX(`label`);
 
 
 -- DATA
@@ -42,42 +41,44 @@ TRUNCATE `cinergie`.`merchandise`;
 -- INSERT BOOKS
 INSERT INTO `cinergie`.`merchandise` (
   `id`,
+  `created`,
 
-  `created_on`,
   `slug`,
-  `rank`,
-  `isActive`,
 
   `label`,
+  `rank`,
+
+  `avatar`,
   `content`,
+
+  `public`,
+
+  `isBook`,
 
   `people`,
   `price`,
-
-  `profilePicture`,
-    
-  `isBook`,
 
   `legacy_id`,
   `legacy_lien`
 )
 SELECT
   CAST(REGEXP_SUBSTR(`id`, '[0-9]+$', 1) as UNSIGNED) as `id`,
+  STR_TO_DATE(datestamp,'%Y-%m-%d %H:%i:%s') as `created`,
 
-  STR_TO_DATE(datestamp,'%Y-%m-%d %H:%i:%s') as `created_on`,
   urlparms as `slug`,
+
+  TRIM(field01) as `label`,
   tri as `rank`,
-  `active` as `isActive`,
 
-  field01 as `label`,
-  field03 as `content`,
+  field06 as `avatar`,
+  TRIM(field03) as `content`,
 
-  field02 as `people`,
-  field04 as `price`,
-
-  field06 as `profilePicture`,
+  `active` as `public`,
 
   1 as `isBook`,
+
+  TRIM(field02) as `people`,
+  field04 as `price`,
 
   id as `legacy_id`,
   field05 as `legacy_lien`
@@ -90,16 +91,16 @@ WHERE area = 'ventedvd' AND category = 'livre';
 -- INSERT DVDs
 
 INSERT INTO `cinergie`.`merchandise` (
-  `isActive`,
   `slug`,
   `label`,
+  `public`,
   `price`,
   `isBook`
 )
 SELECT
-  `ventedvdok`,
   `urlparms`,
   `nom`,
+  `ventedvdok`,
   `ventedvdprix`,
   0 as `isBook`
 FROM `a7_cinergie_beta`.`film`
