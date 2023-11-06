9b2cf0b897fc09f93c3858cecc4e75c03c50fe90
HexMakina <20891495+HexMakina@users.noreply.github.com>
2023-11-06 23:54:20 +0100

    forgot: movie_tag becomes movie_theme

--- import/001_legacy_migration/406_movie_tag.sql
+++ import/001_legacy_migration/406_movie_tag.sql
@@ -1,41 +1,41 @@
--- RELATION movie_tag
-DROP TABLE IF EXISTS `cinergie`.`movie_tag`;
+-- RELATION movie_theme
+DROP TABLE IF EXISTS `cinergie`.`movie_theme`;
 
-CREATE TABLE `cinergie`.`movie_tag` (
+CREATE TABLE `cinergie`.`movie_theme` (
   `movie_id` int NOT NULL,
   `tag_id` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- Create an AI id to delete duplicates
-ALTER TABLE `movie_tag`
+ALTER TABLE `movie_theme`
   ADD `id` INT NOT NULL AUTO_INCREMENT FIRST,
   ADD PRIMARY KEY (`id`);
 
 
 -- Data insert
-INSERT INTO `cinergie`.`movie_tag` (`movie_id`,`tag_id`)
+INSERT INTO `cinergie`.`movie_theme` (`movie_id`,`tag_id`)
 SELECT `film`.`id` as `movie_id`, `itm_theme`.`id` as `tag_id`
 FROM `a7_cinergie_beta`.`film`
 INNER JOIN `cinergie`.`tag` `itm_theme` ON `itm_theme`.`slug` = `film`.`theme`;
 
-INSERT INTO `cinergie`.`movie_tag` (`movie_id`,`tag_id`)
+INSERT INTO `cinergie`.`movie_theme` (`movie_id`,`tag_id`)
 SELECT `film`.`id` as `movie_id`, `itm_theme`.`id` as `tag_id`
 FROM `a7_cinergie_beta`.`film`
 INNER JOIN `cinergie`.`tag` `itm_theme` ON `itm_theme`.`slug` = `film`.`theme_bis`;
 
-INSERT INTO `cinergie`.`movie_tag` (`movie_id`,`tag_id`)
+INSERT INTO `cinergie`.`movie_theme` (`movie_id`,`tag_id`)
 SELECT `film`.`id` as `movie_id`, `itm_theme`.`id` as `tag_id`
 FROM `a7_cinergie_beta`.`film`
 INNER JOIN `cinergie`.`tag` `itm_theme` ON `itm_theme`.`slug` = `film`.`theme_ter`;
 
 
 -- Cleanup double theme
-DELETE FROM `cinergie`.`movie_tag`
+DELETE FROM `cinergie`.`movie_theme`
 WHERE `movie_id` IN (
   SELECT `movie_id`
   FROM (
     SELECT `movie_id`, `tag_id`
-    FROM `cinergie`.`movie_tag`
+    FROM `cinergie`.`movie_theme`
     GROUP BY `movie_id`, `tag_id`
     HAVING COUNT(*) > 1
   ) AS `duplicates`
@@ -44,7 +44,7 @@ AND `tag_id` IN (
   SELECT `tag_id`
   FROM (
     SELECT `movie_id`, `tag_id`
-    FROM `cinergie`.`movie_tag`
+    FROM `cinergie`.`movie_theme`
     GROUP BY `movie_id`, `tag_id`
     HAVING COUNT(*) > 1
   ) AS `duplicates`
@@ -53,11 +53,11 @@ AND `tag_id` IN (
 
 
 -- unique, index & FK
-ALTER TABLE `movie_tag`
-  ADD UNIQUE KEY `movie_tag-isUnique` (`movie_id`, `tag_id`),
-  ADD KEY `movie_tag-hasMovie` (`movie_id`),
-  ADD KEY `movie_tag-hasTag` (`tag_id`);
+ALTER TABLE `movie_theme`
+  ADD UNIQUE KEY `movie_theme-isUnique` (`movie_id`, `tag_id`),
+  ADD KEY `movie_theme-hasMovie` (`movie_id`),
+  ADD KEY `movie_theme-hasTag` (`tag_id`);
 
-ALTER TABLE `movie_tag`
-  ADD CONSTRAINT `movie_tag-hasMovie` FOREIGN KEY (`movie_id`) REFERENCES `movie` (`id`),
-  ADD CONSTRAINT `movie_tag-hasTag` FOREIGN KEY (`tag_id`) REFERENCES `tag` (`id`);
+ALTER TABLE `movie_theme`
+  ADD CONSTRAINT `movie_theme-hasMovie` FOREIGN KEY (`movie_id`) REFERENCES `movie` (`id`),
+  ADD CONSTRAINT `movie_theme-hasTag` FOREIGN KEY (`tag_id`) REFERENCES `tag` (`id`);
