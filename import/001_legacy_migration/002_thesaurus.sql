bc24f8e63386ab73dd7a734d4926fbc2840167f4
HexMakina <20891495+HexMakina@users.noreply.github.com>
2023-11-06 02:34:59 +0100

    cleanup

--- import/001_legacy_migration/002_thesaurus.sql
+++ import/001_legacy_migration/002_thesaurus.sql
@@ -2,45 +2,41 @@
 DROP TABLE IF EXISTS `thesaurus`;
 
 CREATE TABLE `thesaurus` (
-  `id` int NOT NULL,
-  
-  `created_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `active` tinyint(1) NOT NULL DEFAULT '0',
-  `slug` varchar(222) DEFAULT NULL COMMENT 'leg:urlparm',
-
+  `id` int NOT NULL AUTO_INCREMENT,
   `label` varchar(255) NOT NULL,
+  
+  `active` tinyint(1) NOT NULL DEFAULT '0',
+  `slug` varchar(222) DEFAULT NULL COMMENT 'leg:urlparms',
 
-  `legacy_id` varchar(255) DEFAULT NULL
+  `legacy_id` varchar(255) DEFAULT NULL,
+
+  PRIMARY KEY (`id`),
+  INDEX(`label`)
+
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='UNESCO';
 
--- PRIMARY
-ALTER TABLE `thesaurus` ADD PRIMARY KEY (`id`);
-ALTER TABLE `thesaurus` MODIFY `id` int NOT NULL AUTO_INCREMENT;
+ALTER TABLE `thesaurus` ADD UNIQUE KEY `thesaurus-unique-slug` (`slug`) USING BTREE;
 
--- INDEX
-ALTER TABLE `thesaurus` ADD UNIQUE `thesaurus-slug-unique` (`slug`);
-ALTER TABLE `thesaurus` ADD INDEX(`label`);
 
 -- DATA
 TRUNCATE `thesaurus`;
 
 INSERT INTO `cinergie`.`thesaurus` (
-  `created_on`,
+  `label`,
+
   `active`,
   `slug`,
-
-  `label`,
 
   `legacy_id`
 )
 SELECT
-  STR_TO_DATE(datestamp,'%Y-%m-%d %H:%i:%s') as `created_on`,
+  title as `label`,
+
   `active` as `active`,
   IF(urlparms='', null, urlparms) as `slug`,
 
-  title as `label`,
-
   id as `legacy_id`
+
 FROM `a7_cinergie_beta`.`content_item`
 WHERE area = 'autorite' AND category = 'autorite'
 ORDER BY title ASC;
