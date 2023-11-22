804d99a49f88e5492b434b3847a5ab10fee495e2
HexMakina <20891495+HexMakina@users.noreply.github.com>
2023-11-22 19:00:15 +0100

    new partial: article card

---
+++ app/Views/Open/Article/card.php
@@ -0,0 +1,12 @@
+<article class="card mb-4 shadow">
+
+    <a href="article-single.php">
+        <img src="<?= $article->profilePicture() ?>" class="card-img-top" alt="<?= $article->get('label'); ?> - <?= $article->get('author_label'); ?>"" />
+        <div class="card-body">
+            <p class="btn btn-sm btn-primary taxo-cat">Catégorie</p>
+            <p class="date otto-date"><?= $article->get('publication'); ?></p>
+            <h5 class="card-title"><?= $article->get('label'); ?></h5>
+            <span class="cta cta-card">Lire l'article</span>
+        </div>
+    </a>
+</article>
