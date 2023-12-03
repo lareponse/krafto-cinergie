fa96b8bdbf2d77d0ff7fb4f1c387b7ef3cd57f75
HexMakina <20891495+HexMakina@users.noreply.github.com>
2023-12-03 17:28:51 +0100

    movie filters toggle

--- app/Views/Open/Movie/form_filters.php
+++ app/Views/Open/Movie/form_filters.php
@@ -1,4 +1,6 @@
-<?php use \HexMakina\Marker\Form; ?>
+<?php
+
+use \HexMakina\Marker\Form; ?>
 <form action="<?= $controller->router()->hyp('movies'); ?>" method="GET">
 
     <header>
@@ -31,23 +33,31 @@
         <?= Form::options($themes, $controller->router()->params('theme')); ?>
     </select>
 
+        <a href="#" class="mt-3 d-block text-secondary" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+            <i class="bi bi-chevron-down"></i>
+            D'autres critères ? 
+        </a>
+    <div class="collapse" id="collapseExample">
+        <hr class="mb-1">
 
-    <hr class="mt-4 mb-1">
+        <label class="form-label mt-3">Titre</label>
+        <input class="form-control" type="text" placeholder="Titre" name="label" value="<?= $controller->router()->params('label') ?>" />
 
-    <label class="form-label mt-3">Titre</label>
-    <input class="form-control" type="text" placeholder="Titre" name="label" value="<?= $controller->router()->params('label') ?>" />
+        <label class="form-label mt-3">Réalisateur</label>
+        <input class="form-control" type="text" placeholder="Réalisateur" name="director" value="<?= $controller->router()->params('director') ?>" />
 
-    <label class="form-label mt-3">Réalisateur</label>
-    <input class="form-control" type="text" placeholder="Réalisateur" name="director" value="<?= $controller->router()->params('director') ?>" />
+        <label class="form-label mt-3">Organisation</label>
+        <input class="form-control" type="text" placeholder="Organisation" name="organisation" value="<?= $controller->router()->params('organisation') ?>" />
 
-    <label class="form-label mt-3">Organisation</label>
-    <input class="form-control" type="text" placeholder="Organisation" name="organisation" value="<?= $controller->router()->params('organisation') ?>" />
+        <label class="form-label mt-3">Professionnel</label>
+        <input class="form-control" type="text" placeholder="Professionnel" name="professional" value="<?= $controller->router()->params('professional') ?>" />
 
-    <label class="form-label mt-3">Professionnel</label>
-    <input class="form-control" type="text" placeholder="Professionnel" name="professional" value="<?= $controller->router()->params('professional') ?>" />
+        <label class="form-label mt-3">Année de sortie</label>
+        <input class="form-control" type="text" placeholder="Année de sortie" name="released" value="<?= $controller->router()->params('released') ?>" />
 
-    <label class="form-label mt-3">Année de sortie</label>
-    <input class="form-control" type="text" placeholder="Année de sortie" name="released" value="<?= $controller->router()->params('released') ?>" />
+    </div>
+
+
 
     <hr>
 
