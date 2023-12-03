bd9ee1ff26f6b9f2247bc41bf2ddc9aff858bddc
HexMakina <20891495+HexMakina@users.noreply.github.com>
2023-12-03 17:49:44 +0100

    integrated partial filmotheque

--- app/Views/Open/Professional/filmotheque.php
+++ app/Views/Open/Professional/filmotheque.php
@@ -1,40 +1,28 @@
 <?php
 if (!empty($movies)) {
 ?>
-    <section class="my-5" id="filmographie">
+
+    <div class="my-5 filmographie">
         <h2 class="pb-0">Filmothèque</h2>
         <hr />
-        <div id="filmotheque">
-            <div class="slide arrow" id="single-post-slider">
-                <?php
+        <div class="filmotheque">
+            <div class="slide arrow single-post-slider">
+            <?php
                 foreach ($movies as $movie) {
                     $href = $controller->router()->hyp('movie', ['slug' => $movie->slug()]);
-                ?>
-                    <div class="col-lg-4" id="film-item">
-                        <article class="card shadow">
-                            <div class="card-body">
-                                <a href="<?= $href ?>">
-                                    <img src="<?= $movie->profilePicture() ?>" class="card-img-top mb-3" alt="...">
-                                </a>
-                                <div class="p-3">
-                                    <a href="<?= $href ?>">
-                                        <div class="meta">
-                                            <p class="date"><small class="text-secondary"><?= $movie->get('released') ?></small></p>
-                                            <p class="categorie"><small class="text-primary" class="otto-id-label" otto-urn="Tag:<?= $movie->get('genre_id')?>"><?= $movie->get('genre_id')?></small></p>
-                                        </div>
-                                        <h5 class="card-title"><?= $movie ?></h5>
-                                        <p class="auteur"><?= $movie->get('directors') ?></p>
-                                    </a>
-                                </div>
-                            </div>
-                        </article>
-                    </div>
+            ?>
+
+                <div class="col-lg-4 film-item">
+                    
+                <?= $this->insert('Open::Movie/card', ['record' => $movie]) ?>
+
+                </div>
                 <?php
                 }
                 ?>
             </div>
         </div>
-    </section>
+    </div>
 <?php
 }
 ?>
