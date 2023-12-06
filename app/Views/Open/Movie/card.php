1f90057f24efebc2cfc5d61ec9ebfc46248b0d80
HexMakina <20891495+HexMakina@users.noreply.github.com>
2023-12-06 01:42:30 +0100

    improved movie card

--- app/Views/Open/Movie/card.php
+++ app/Views/Open/Movie/card.php
@@ -5,16 +5,17 @@ $url = $controller->router()->hyp('movie', ['slug' => $record->slug()]);
 <article class="film-item card shadow">
     <div class="card-body">
         <a href="<?= $url ?>">
-            <img src="<?=$record->profilePicture()?>" class="card-img-top mb-3" alt="Couverture du film <?= $record->get('label');?>">
+            <img src="<?= $record->profilePicture() ?>" class="card-img-top mb-3" alt="Couverture du film <?= $record->get('label'); ?>">
         </a>
-        <div class="px-3">
+        <div class="px-3  mb-3">
             <a href="<?= $url ?>">
-                <div class="meta d-flex justify-content-between mb-3">
-                    <small class="date text-secondary"><?= $record->get('released');?></small>
-                    <small class="text-primary categorie otto-id-label" otto-urn="Tag:<?= $record->get('genre_id');?>"><?= $record->get('genre_id');?></small>
+                <div class="d-flex justify-content-between">
+                    <small class="date text-secondary"><?= $record->get('released'); ?></small>
+                    <small class="text-secondary categorie otto-id-label" otto-urn="Tag:<?= $record->get('genre_id'); ?>"><?= $record->get('genre_id'); ?></small>
                 </div>
-                <h5 class="card-title"><?= $record->get('label'); ?></h5>
-                <p class="auteur"><?= $record->get('directors'); ?></p>
+
+                <h5 class="card-title text-center"><?= $record->get('label'); ?></h5>
+                <span class="auteur d-block"><?= $record->get('directors'); ?></span>
             </a>
         </div>
     </div>
