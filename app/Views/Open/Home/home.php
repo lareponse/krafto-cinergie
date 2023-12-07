634a2a9fc18b7942c2569429f3462ca43007a6dd
HexMakina <20891495+HexMakina@users.noreply.github.com>
2023-12-07 23:01:08 +0100

    merged div id="banner" and class="container-custom-banner

--- app/Views/Open/Home/home.php
+++ app/Views/Open/Home/home.php
@@ -1,30 +1,28 @@
 <?php $this->layout('Open::layout') ?>
 
-<div class="container-custom-banner">
-    <div id="banner">
-        <?php
-        foreach ($articlesDiaporama as $article) {
-            $href = $controller->router()->hyp('article', ['slug' => $article->slug()]);
+<div id="banner" class="container-custom-banner">
+    <?php
+    foreach ($articlesDiaporama as $article) {
+        $href = $controller->router()->hyp('article', ['slug' => $article->slug()]);
 
-        ?>
-            <article class="card slide">
-                <img src="<?= $article->profilePicture() ?>" class="card-img" alt="<?= $article->get('label'); ?> - <?= $article->get('author_label'); ?>">
-                <div class="card-img-overlay">
-                    <h3 class="card-title h1"><?= $article->get('label'); ?></h3>
-                    <p class="card-text"><small class="text-primary"><?= $article->get('author_label'); ?></small></p>
-                    <p class="card-text">
-                        <?= substr(strip_tags($article->get('abstract')), 0, 400); ?>...
-                    </p>
-                    <p><a href="<?= $href ?>" class="btn btn-primary">En savoir plus</a></p>
-                </div>
-                <div class="card carrousel card-img-overlay">
-                </div>
-            </article>
-        <?php
-        }
-        ?>
+    ?>
+        <article class="card slide">
+            <img src="<?= $article->profilePicture() ?>" class="card-img" alt="<?= $article->get('label'); ?> - <?= $article->get('author_label'); ?>">
+            <div class="card-img-overlay">
+                <h3 class="card-title h1"><?= $article->get('label'); ?></h3>
+                <p class="card-text"><small class="text-primary"><?= $article->get('author_label'); ?></small></p>
+                <p class="card-text">
+                    <?= substr(strip_tags($article->get('abstract')), 0, 400); ?>...
+                </p>
+                <p><a href="<?= $href ?>" class="btn btn-primary">En savoir plus</a></p>
+            </div>
+            <div class="card carrousel card-img-overlay">
+            </div>
+        </article>
+    <?php
+    }
+    ?>
 
-    </div>
 </div>
 
 
@@ -42,6 +40,7 @@
         $this->insert('Open::Home/section_loupe', ['sousLaLoupe' => $sousLaLoupe]);
     }
     ?>
+</div>
 </div>
 
 <div class="d-lg-block bg-light py-5 mt-5" id="home-rdv-annonce">
