57cf150f648958ac41dd3eee476363bc4b039327
HexMakina <20891495+HexMakina@users.noreply.github.com>
2023-12-07 23:54:42 +0100

    fixed navbar

--- app/Views/Open/_partials/header.php
+++ app/Views/Open/_partials/header.php
@@ -1,52 +1,19 @@
 <header class="navbar navbar-expand-xl py-3" id="header">
     <div class="container">
 
-        <a class="navbar-brand" href="<?=$controller->router()->hyp('home')?>">
-            <img src="/public/assets/img/logo-cinergie.svg" alt="Logo CINERGIE" id="logo">
+        <a class="navbar-brand" href="<?= $controller->router()->hyp('home') ?>">
+            <img src="/public/assets/img/logo-cinergie.svg" alt="Logo CINERGIE" class="logo">
         </a>
-
         <div class="socials d-block d-xl-none ms-auto">
-            <a type="button" data-bs-toggle="collapse" data-bs-target="#searchBAR" aria-controls="searchBAR" aria-expanded="false" aria-label="Toggle navigation" href="#" class="mx-1">
-                <img src="/public/assets/wejune/img/icons/search.svg" alt="Recherche sur le site" />
-            </a>
+            <button type="button" data-bs-toggle="collapse" data-bs-target="#searchBAR" aria-controls="searchBAR" aria-expanded="false" aria-label="Toggle navigation" class="mx-1 btn-search">
+                <img src="/public/assets/wejune/img/icons/search.svg" alt="">
+            </button>
         </div>
 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
             <span class="bi bi-filter-right"></span>
         </button>
+        <?= $this->insert('Open::_partials/navbar') ?>
 
-        <nav class="collapse navbar-collapse" id="navigation">
-            <ul class="navbar-nav mx-auto">
-                <?php $activeSection = $controller->activeSection()?>
-
-                <li class="nav-item">
-                    <a class="nav-link <?= $activeSection == 'Article' ? 'active' : '' ?>" href="<?= $controller->router()->hyp('articles'); ?>">Articles</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link <?= $activeSection == 'Event' ? 'active' : '' ?>" href="<?= $controller->router()->hyp('events'); ?>">Agenda</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link <?= $activeSection == 'Movie' ? 'active' : '' ?>" href="<?= $controller->router()->hyp('movies'); ?>">Filmoth&egrave;que</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link <?= $activeSection == 'Professional' ? 'active' : '' ?>" href="<?= $controller->router()->hyp('professionals'); ?>">Professionnels</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link <?= $activeSection == 'Organisation' ? 'active' : '' ?>" href="<?= $controller->router()->hyp('organisations'); ?>">Organisations</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link <?= $activeSection == 'Job' ? 'active' : '' ?>" href="<?= $controller->router()->hyp('jobs'); ?>">Castings & Jobs</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link <?= $activeSection == 'Shop' ? 'active' : '' ?>" href="<?= $controller->router()->hyp('shop'); ?>">Boutique</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="https://www.cinergie.be/podcast">Podcast</a>
-                </li>
-            </ul>
-            <div class="socials d-none d-xl-block">
-                <a type="button" data-bs-toggle="collapse" data-bs-target="#searchBAR" aria-controls="searchBAR" aria-expanded="false" aria-label="Toggle navigation" href="#" class="mx-1"><img src="/public/assets/wejune/img/icons/search.svg" alt="Recherche sur le site" /></a>
-            </div>
-        </nav>
     </div>
 </header>
---
+++ app/Views/Open/_partials/navbar.php
@@ -0,0 +1,64 @@
+<?php
+$activeSection = $actionSection ?? $controller->activeSection();
+
+$menu = [
+    [
+        'section' => 'Article',
+        'href' => $controller->router()->hyp('articles'),
+        'label' => 'Articles'
+    ],
+    [
+        'section' => 'Event',
+        'href' => $controller->router()->hyp('events'),
+        'label' => 'Agenda'
+    ],
+    [
+        'section' => 'Movie',
+        'href' => $controller->router()->hyp('movies'),
+        'label' => 'Filmoth&egrave;que'
+    ],
+    [
+        'section' => 'Professional',
+        'href' => $controller->router()->hyp('professionals'),
+        'label' => 'Professionnels'
+    ],
+    [
+        'section' => 'Organisation',
+        'href' => $controller->router()->hyp('organisations'),
+        'label' => 'Organisations'
+    ],
+    [
+        'section' => 'Job',
+        'href' => $controller->router()->hyp('jobs'),
+        'label' => 'Castings & Jobs'
+    ],
+    [
+        'section' => 'Shop',
+        'href' => $controller->router()->hyp('shop'),
+        'label' => 'Boutique'
+    ],
+    [
+        'section' => 'Podcast',
+        'href' => 'https://www.cinergie.be/podcast',
+        'label' => 'Podcast'
+    ]
+];
+?>
+<nav class="collapse navbar-collapse" id="navigation">
+    <ul class="navbar-nav mx-auto">
+        <?php
+        foreach ($menu as $item) {
+            printf(
+                '<li class="nav-item"><a class="nav-link %s" href="%s">%s</a></li>',
+                $activeSection == $item['section'] ? 'active' : '',
+                $item['href'],
+                $item['label']
+            );
+        }
+        ?>
+    </ul>
+    <div class="socials d-none d-xl-block">
+        <button type="button" data-bs-toggle="collapse" data-bs-target="#searchBAR" aria-controls="searchBAR" aria-expanded="false" aria-label="Toggle navigation" class="mx-1 btn-search">
+            <img src="/public/assets/wejune/img/icons/search.svg" alt="Search bar toggler"></button>
+    </div>
+</nav>
