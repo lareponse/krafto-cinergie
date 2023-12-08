487d4a2f780437500c3941c2bab58a9b40ccc8bb
HexMakina <20891495+HexMakina@users.noreply.github.com>
2023-12-08 03:47:25 +0100

    restored container

--- app/Views/Open/Article/articles.php
+++ app/Views/Open/Article/articles.php
@@ -1,22 +1,22 @@
 <?php $this->layout('Open::layout', ['title' => "Toute l'actualité du cinéma belge"]) ?>
+<div class="container">
+    <div class="mx-auto my-5 w-75">
+        <?= $this->insert('Open::Article/form_search', [
+                'action' => $controller->router()->hyp('articles'),
+                'value' => $controller->router()->params('s')
+        ]) ?>
+    </div>
 
-<div class="mx-auto my-5 w-75">
-    <?= $this->insert('Open::Article/form_search', [
-        'action' => $controller->router()->hyp('articles'),
-        'value' => $controller->router()->params('s')
-    ]) ?>
-</div>
-
-<section class="row my-5" id="articles">
-    <?php
-    foreach ($paginator->records() as $article) {
-    ?>
-        <div class="col-lg-4">
-            <?= $this->insert('Open::Article/card', ['article' => $article]) ?>
-        </div>
-    <?php
-    }
-    ?>
-</section>
-
+    <div class="row my-5" id="articles">
+        <?php
+        foreach ($paginator->records() as $article) {
+        ?>
+            <div class="col-lg-4">
+                <?= $this->insert('Open::Article/card', ['article' => $article]) ?>
+            </div>
+        <?php
+        }
+        ?>
+    </div>
 <?= $this->insert('Open::_partials/pagination', ['route' => 'articles', 'paginator' => $paginator]); ?>
+</div>
