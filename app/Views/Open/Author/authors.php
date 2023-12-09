414bd6735535fb05af8516bef301dec96a423855
HexMakina <20891495+HexMakina@users.noreply.github.com>
2023-12-10 00:11:55 +0100

    container added

--- app/Views/Open/Author/authors.php
+++ app/Views/Open/Author/authors.php
@@ -1,18 +1,19 @@
 <?php $this->layout('Open::layout', ['title' => $page->get('label')]) ?>
-
-<div class="row my-5 pb-5 auteurs">
-    <?php foreach ($authors as $author) { ?>
-        <div class="col-lg-3 col-md-6 auteur-item">
-            <article class="card mb-4 shadow">
-                <a href="<?= $controller->router()->hyp('author', ['slug' => $author->slug()]); ?>">
-                    <div class="card-body">
-                        <img src="<?= $author->profilePicture(); ?>" class="card-img-top mb-3" alt="Photo de <?= $author->fullName() ?>" />
-                        <div class="p-3">
-                            <h5 class="card-title text-center"><?= $author->fullName(); ?></h5>
+<div class="container">
+    <div class="row my-5 pb-5 auteurs">
+        <?php foreach ($authors as $author) { ?>
+            <div class="col-lg-3 col-md-6 auteur-item">
+                <article class="card mb-4 shadow">
+                    <a href="<?= $controller->router()->hyp('author', ['slug' => $author->slug()]); ?>">
+                        <div class="card-body">
+                            <img src="<?= $author->profilePicture(); ?>" class="card-img-top mb-3" alt="Photo de <?= $author->fullName() ?>" />
+                            <div class="p-3">
+                                <h5 class="card-title text-center"><?= $author->fullName(); ?></h5>
+                            </div>
                         </div>
-                    </div>
-                </a>
-            </article>
-        </div>
-    <?php } ?>
+                    </a>
+                </article>
+            </div>
+        <?php } ?>
+    </div>
 </div>
