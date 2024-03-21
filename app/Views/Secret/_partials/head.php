237c302638920986d547fa73cb2de76d4064db50
HexMakina <20891495+HexMakina@users.noreply.github.com>
2024-03-21 15:06:35 +0100

    removed OpenSans font

--- app/Views/Secret/_partials/head.php
+++ app/Views/Secret/_partials/head.php
@@ -1,24 +1,17 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta content="Webinning" name="author">
+<meta name="description" content="Cinergie.be | Dashboard">
 
 <!-- Theme CSS -->
 <link rel="stylesheet" href="/public/assets/dashly/css/theme.bundle.css" id="stylesheetLTR">
 <link rel="stylesheet" href="/public/assets/dashly/css/theme.rtl.bundle.css" id="stylesheetRTL">
 <link rel="stylesheet" href="/public/assets/css/krafto_cinergie.css">
 
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700;800&display=swap">
-<link rel="stylesheet" media="print" onload="this.onload=null;this.removeAttribute('media');" href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700;800&display=swap">
-
-<!-- no-JS fallback -->
-<noscript>
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700;800&display=swap">
-</noscript>
+<!-- Page Title -->
+<title>Cinergie.be | Dashboard</title>
 
 <script>
-// Theme switcher
-
+    // Theme switcher
   let themeSwitcher = document.getElementById('themeSwitcher');
 
   const getPreferredTheme = () => {
@@ -118,6 +111,3 @@
         html.removeAttribute('dir');
     }
 </script>
-
-<!-- Page Title -->
-<title>Cinergie.be | Dashboard</title>
