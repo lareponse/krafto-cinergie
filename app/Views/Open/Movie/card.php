13f266465ccf3d4baf90d42fbeed7738547ffa57
HexMakina <20891495+HexMakina@users.noreply.github.com>
2024-03-24 00:05:57 +0100

    trimed the bootstrap fat for mov and org card, new movie card black

--- app/Views/Open/Movie/card.php
+++ app/Views/Open/Movie/card.php
@@ -2,23 +2,18 @@
 $url = $controller->router()->hyp('movie', ['slug' => $record->slug()]);
 
 ?>
-<article class="film-item card shadow">
-    <div class="card-body">
-        <a href="<?= $url ?>">
+        <a class="card-complex" href="<?= $url ?>">
             <img src="<?= $record->profilePicture() ?>" class="card-img-top" alt="Couverture du film <?= $record->get('label'); ?>">
-        </a>
-            <a class="d-block px-3" href="<?= $url ?>">
-                <div class="d-flex justify-content-between">
-                    <small class="date text-secondary"><?= $record->get('released'); ?></small>
-                    <small class="text-primary categorie otto-id-label" otto-urn="Tag:<?= $record->get('genre_id'); ?>"><?= $record->get('genre_id'); ?></small>
-                </div>
+        
+            <small class="date text-secondary"><?= $record->get('released'); ?></small>
+            <small class="categorie otto-id-label" otto-urn="Tag:<?= $record->get('genre_id'); ?>"><?= $record->get('genre_id'); ?></small>
 
-                <h5 class="card-title"><?= $record->get('label'); ?></h5>
-                <?php
-                if(!empty($record->get('directors'))){
-                    echo $this->DOM()::span('de '.$record->get('directors'), ['class' => 'd-block text-end']);
-                }
-                ?>
-            </a>
-    </div>
-</article>
+            <h5 class="card-title"><?= $record->get('label'); ?></h5>
+            <?php
+            // if (!empty($record->get('directors'))) {
+            //     echo $this->DOM()::span('de ' . $record->get('directors'), ['class' => 'd-block text-end']);
+            // }
+            ?>
+        </a>
+    <!-- </div>
+</article> -->
--- app/Views/Open/Organisation/card.php
+++ app/Views/Open/Organisation/card.php
@@ -1,7 +1,7 @@
 <?php
 $href = $href ?? $controller->router()->hyp('organisation', ['slug' => $record->slug()]);
 ?>
-<article class="card mb-4 shadow">
+<!-- <article class="card mb-4 shadow">
     <div class="card-body text-center">
         <a href="<?= $href ?>">
             <img src="<?= $record->profilePicture(); ?>" class="card-img-top mb-3" alt="Photo <?= $record->get('label'); ?>" />
@@ -9,4 +9,16 @@ $href = $href ?? $controller->router()->hyp('organisation', ['slug' => $record->
             <p class="card-text mt-3"><small class="text-secondary"><?= $record->get('praxes'); ?></small></p>
         </a>
     </div>
-</article>
+</article> -->
+
+<a class="card mb-4 shadow" href="<?= $href ?>">
+    <img src="<?= $record->profilePicture(); ?>" class="card-img-top mb-3" alt="Photo <?= $record->get('label'); ?>" />
+    <h5 class="card-title p-2 text-center"><?= $record->get('label'); ?></h5>
+    <p class="card-text p-2 text-center">
+        <?php
+        foreach ($record->praxisIds() as $id) {
+            printf('<small class="comma text-secondary otto-id-label" otto-urn="Tag:%d">Tag:%d</small>', $id, $id);
+        }
+        ?>
+    </p>
+</a>
