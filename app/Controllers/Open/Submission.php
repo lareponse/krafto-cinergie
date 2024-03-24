4a30516485528666372817bbec9adb4d18af382c
HexMakina <20891495+HexMakina@users.noreply.github.com>
2024-03-25 00:42:13 +0100

    boutique submission

--- app/Controllers/Open/Submission.php
+++ app/Controllers/Open/Submission.php
@@ -9,38 +9,38 @@ class Submission extends Kortex
     // The submit method is called when a form is submitted
     public function submit()
     {
+        $submission = new Model();
+        $submission->set('submitted', json_encode($this->router()->submitted()));
+
+
         $res = explode('/', $this->router()->referer());
 
-        list($type, $slug) = array_slice($res, -2);
+        // no slug
+        if (!isset($res[4])) {
+            $page = $res[3];
+            $submission->set('urn', $page);
+        } else {
 
-        $new = $this->get('\\App\\Models\\' . ucfirst($type));
-        
-        // deny the request if the record type is not found
-        if(!$new){
-            $this->router()->hopBack();
-        }
-        
-        // $new->import($this->router()->submitted());
+            list($type, $slug) = array_slice($res, -2);
+            try {
+                $type = ucfirst($type);
+                $new = $this->get('\\App\\Models\\' . ucfirst($type));
+            } catch (\Exception $e) {
+                $this->router()->hopBack();
+            }
 
-        
-        // deny the request if the record is not found through the slug
-        $existing = get_class($new)::exists(['slug' => $slug]);
-        if(!$existing){
-            $this->router()->hopBack();
+            // deny the request if the record is not found through the slug
+            $existing = get_class($new)::exists(['slug' => $slug]);
+            if (!$existing) {
+                $this->router()->hopBack();
+            }
+
+            // now we have a verified slug and a record, we can proceed with the form
+            $submission->set('urn', $existing->urn());
         }
-        
-        // now we have a verified slug and a record, we can proceed with the form
-        $submission = new Model();
-        $submission->set('urn', $existing->urn());
-        $submission->set('submitted', json_encode($this->router()->submitted()));
+
         $submission->save(0);
 
         $this->viewport('submission', $submission);
-        // $this->router()->hop('submission_confirmed', ['urn' => $submission->urn()]);
-    }
-
-    public function confirmed()
-    {
-
     }
 }
--- app/Views/Open/Merchandise/modal_order.php
+++ app/Views/Open/Merchandise/modal_order.php
@@ -1,33 +1,73 @@
 <!-- Modal -->
-<div class="modal fade" id="modal-order" tabindex="-1" aria-labelledby="modal-nouvelle-commande-label-dvd" aria-hidden="true">
-    <div class="modal-dialog modal-xl">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h2 class="modal-title fs-5" id="modal-nouvelle-commande-label-dvd">Nouvelle commande</h2>
-                <button type="button" class="btn-close  btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <div class="row">
+<form class="submission-form" id="nouvelle-commande-dvd" method="POST" action="<?= $controller->router()->hyp('submission_submit');?>">
+
+    <div class="modal fade" id="modal-order" tabindex="-1" aria-labelledby="modal-nouvelle-commande-label-dvd" aria-hidden="true">
+        <div class="modal-dialog modal-lg modal-dialog-scrollable">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h2 class="modal-title fs-5" id="modal-nouvelle-commande-label-dvd">Nouvelle commande</h2>
+                    <button type="button" class="btn-close  btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    
                     <h2 class="titrefilm modal-titre text-right"></h2>
                     <h3 class="prix-film modal-prix text-right"></h3>
-                </div>
-                <div class="row">
-                    <div class="col-lg-8">
-                        <?= $this->insert('Open::Merchandise/form_order') ?>
-                    </div>
-                    <div class="col">
+                    
+                    <div>
                         <strong class="h4">Comment commander?</strong>
                         <p>Pour commander un DVD ou un livre, il vous suffit de remplir ce formulaire avec vos nom, prénom, adresse, numéro de téléphone, etc..</p>
                         <p>Les frais d'envoi <strong>en Belgique</strong> sont compris dans le prix du DVD/livre. Les frais d'envoi hors frontières sont calculés selon la destination (Europe +8€).</p>
                         <p>Le numéro de compte bancaire de Cinergie asbl est:&nbsp;<br> IBAN : BE10 0012 4446 1904<br>BIC : GEBABEBB</p>
                     </div>
+
+
+                    <h4>Contact</h4>
+                    <fieldset>
+                        <label for="nom">Nom</label>
+                        <input type="text" name="nom" value="" id="nom" class="form-control" minlength="2" required="required">
+
+                        <label for="prenom">Prénom</label>
+                        <input type="text" name="prenom" value="" id="prenom" class="form-control" minlength="2" required="required">
+
+                        <label for="telephone">Téléphone</label>
+                        <input type="text" name="telephone" value="" id="telephone" class="form-control" minlength="2" required="required">
+
+                        <label for="email">Email</label>
+                        <input type="email" name="email" value="" id="email" class="form-control" minlength="2" required="required">
+                    </fieldset>
+
+
+                    <h4 class="mt-5">Expédition</h4>
+                    <fieldset>
+                        <label for="adresse">Adresse</label>
+                        <input type="text" name="adresse" value="" id="adresse" class="form-control" minlength="2" required="required">
+
+                        <label for="cp">Code postal</label>
+                        <input type="text" name="cp" value="" id="cp" class="form-control" minlength="2" required="required">
+
+                        <label for="ville">Ville</label>
+                        <input type="text" name="ville" value="" id="ville" class="form-control" minlength="2" required="required">
+
+                        <label for="province">Province</label>
+                        <input type="text" name="province" value="" id="province" class="form-control" minlength="2" required="required">
+
+                        <label for="pays">Pays</label>
+                        <input type="text" name="pays" value="" id="pays" class="form-control" minlength="2" required="required">
+
+                        <label for="commentaire">Commentaire</label>
+                        <textarea name="commentaire" id="commentaire" class="form-control" rows="10"></textarea>
+                    </fieldset>
+                </div>
+                <div class="modal-footer">
+                    <small>Les champs marqués <span style="color:#eb0101; font-size: 30px;"><sub>*</sub></span> sont obligatoires</small>
+                    <input class="btn btn-primary" type="submit" name="submit-nouvelle-annonce" value="Commander">
                 </div>
             </div>
         </div>
     </div>
-</div>
+</form>
 
-<script  nonce="<?= $CSP_nonce?>">
+<script nonce="<?= $CSP_nonce ?>">
     const modal = document.getElementById('modal-order');
     const titreModal = modal.querySelector('.modal-titre');
     const prixModal = modal.querySelector('.modal-prix');
@@ -43,4 +83,3 @@
         });
     });
 </script>
-
