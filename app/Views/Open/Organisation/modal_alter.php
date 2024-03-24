03f8957f2daaac7bf01937e206be5cbcc421ed2d
HexMakina <20891495+HexMakina@users.noreply.github.com>
2024-03-24 22:49:31 +0100

    debootstrapification of pro/org submission form

--- app/Views/Open/Organisation/modal_alter.php
+++ app/Views/Open/Organisation/modal_alter.php
@@ -5,10 +5,9 @@ $title = empty($record) ? 'Nouvelle organisation' : 'Modifier ' . $record;
 ?>
 
 <!-- <class="form-horizontal" id="nouvelle-organisation"> -->
-<form action="<?= $controller->router()->hyp('submission_submit') ?>" method="POST">
-
+<form action="<?= $controller->router()->hyp('submission_submit') ?>" method="POST" class="submission-form">
     <div class="modal fade" id="<?= $modal_reference ?>" tabindex="-1" aria-labelledby="<?= $modal_reference ?>-label" aria-hidden="true">
-        <div class="modal-dialog modal-xl modal-dialog-scrollable">
+        <div class="modal-dialog modal-lg modal-dialog-scrollable">
             <div class="modal-content">
                 <div class="modal-header">
                     <h2 class="modal-title fs-5" id="<?= $modal_reference ?>-label"><?= $title ?></h2>
@@ -24,109 +23,53 @@ $title = empty($record) ? 'Nouvelle organisation' : 'Modifier ' . $record;
 
                     <hr>
 
+                    <fieldset>
+                        <label for="label">Nom <span>*</span></label>
+                        <input type="text" id="label" name="label" value="<?= is_null($record) ? '' : $record->get('label'); ?>" class="form-control" minlength="2" required />
 
-                    <div class="row mb-3">
-                        <label for="label" class="col-xl-3 col-form-label">Nom <span>*</span></label>
-                        <div class="col-xl-9">
-                            <input type="text" id="label" name="label" value="<?= is_null($record) ? '' : $record->get('label'); ?>" class="form-control" minlength="2" required />
-                        </div>
-                    </div>
+                        <label for="abbrev">Abbréviation</label>
+                        <input type="text" id="abbrev" name="abbrev" value="<?= is_null($record) ? '' : $record->get('abbrev'); ?>" class="form-control" minlength="2">
 
-                    <div class="row mb-3">
-                        <label for="abbrev" class="col-xl-3 col-form-label">Abbréviation
-                        </label>
-                        <div class="col-xl-9">
-                            <input type="text" id="abbrev" name="abbrev" value="<?= is_null($record) ? '' : $record->get('abbrev'); ?>" class="form-control" minlength="2">
-                        </div>
-                    </div>
+                        <label for="street">Adresse </label>
+                        <input type="text" id="street" name="street" value="<?= is_null($record) ? '' : $record->get('street'); ?>" class="form-control" minlength="2" />
 
-                    <div class="row mb-3">
-                        <label for="street" class="col-xl-3 col-form-label">Adresse </label>
-                        <div class="col-xl-9">
-                            <input type="text" id="street" name="street" value="<?= is_null($record) ? '' : $record->get('street'); ?>" class="form-control" minlength="2" />
-                        </div>
-                    </div>
+                        <label for="zip">Code postal </label>
+                        <input type="text" id="zip" name="zip" value="<?= is_null($record) ? '' : $record->get('zip'); ?>" class="form-control" minlength="2" />
 
-                    <div class="row mb-3">
-                        <label for="zip" class="col-xl-3 col-form-label">Code postal </label>
-                        <div class="col-xl-9">
-                            <input type="text" id="zip" name="zip" value="<?= is_null($record) ? '' : $record->get('zip'); ?>" class="form-control" minlength="2" />
-                        </div>
-                    </div>
+                        <label for="city">Ville </label>
+                        <input type="text" id="city" name="city" value="<?= is_null($record) ? '' : $record->get('city'); ?>" class="form-control" minlength="2" />
 
-                    <div class="row mb-3">
-                        <label for="city" class="col-xl-3 col-form-label">Ville </label>
-                        <div class="col-xl-9">
-                            <input type="text" id="city" name="city" value="<?= is_null($record) ? '' : $record->get('city'); ?>" class="form-control" minlength="2" />
-                        </div>
-                    </div>
+                        <label for="province">Province</label>
+                        <input type="text" id="province" name="province" value="<?= is_null($record) ? '' : $record->get('province'); ?>" class="form-control" minlength="2" />
 
-                    <div class="row mb-3">
-                        <label for="province" class="col-xl-3 col-form-label">Province
-                        </label>
-                        <div class="col-xl-9">
-                            <input type="text" id="province" name="province" value="<?= is_null($record) ? '' : $record->get('province'); ?>" class="form-control" minlength="2" />
-                        </div>
-                    </div>
+                        <label for="country">Pays </label>
+                        <input type="text" id="country" name="country" value="<?= is_null($record) ? '' : $record->get('country'); ?>" class="form-control" minlength="2" />
 
-                    <div class="row mb-3">
-                        <label for="country" class="col-xl-3 col-form-label">Pays </label>
-                        <div class="col-xl-9">
-                            <input type="text" id="country" name="country" value="<?= is_null($record) ? '' : $record->get('country'); ?>" class="form-control" minlength="2" />
-                        </div>
-                    </div>
+                        <label for="tel">Téléphone</label>
+                        <input type="text" id="tel" name="tel" value="<?= is_null($record) ? '' : $record->get('tel'); ?>" class="form-control" minlength="2" />
 
-                    <div class="row mb-3">
-                        <label for="tel" class="col-xl-3 col-form-label">Téléphone
-                        </label>
-                        <div class="col-xl-9">
-                            <input type="text" id="tel" name="tel" value="<?= is_null($record) ? '' : $record->get('tel'); ?>" class="form-control" minlength="2" />
-                        </div>
-                    </div>
+                        <label for="fax">Fax </label>
+                        <input type="text" id="fax" name="fax" value="<?= is_null($record) ? '' : $record->get('fax'); ?>" class="form-control" minlength="2" />
 
-                    <div class="row mb-3">
-                        <label for="fax" class="col-xl-3 col-form-label">Fax </label>
-                        <div class="col-xl-9">
-                            <input type="text" id="fax" name="fax" value="<?= is_null($record) ? '' : $record->get('fax'); ?>" class="form-control" minlength="2" />
-                        </div>
-                    </div>
+                        <label for="gsm">Gsm </label>
+                        <input type="text" id="gsm" name="gsm" value="<?= is_null($record) ? '' : $record->get('gsm'); ?>" class="form-control" minlength="2" />
 
-                    <div class="row mb-3">
-                        <label for="gsm" class="col-xl-3 col-form-label">Gsm </label>
-                        <div class="col-xl-9">
-                            <input type="text" id="gsm" name="gsm" value="<?= is_null($record) ? '' : $record->get('gsm'); ?>" class="form-control" minlength="2" />
-                        </div>
-                    </div>
+                        <label for="email">E-mail <span>*</span></label>
+                        <input type="text" id="email" name="email" value="<?= is_null($record) ? '' : $record->get('email'); ?>" class="form-control" minlength="2" required />
 
-                    <div class="row mb-3">
-                        <label for="email" class="col-xl-3 col-form-label">E-mail <span>*</span></label>
-                        <div class="col-xl-9">
-                            <input type="text" id="email" name="email" value="<?= is_null($record) ? '' : $record->get('email'); ?>" class="form-control" minlength="2" required />
-                        </div>
-                    </div>
+                        <label for="url">Site </label>
+                        <input type="text" id="url" name="url" value="<?= is_null($record) ? '' : $record->get('url'); ?>" class="form-control" minlength="2" placeholder="site web..." />
 
-                    <div class="row mb-3">
-                        <label for="url" class="col-xl-3 col-form-label">Site </label>
-                        <div class="col-xl-9">
-                            <input type="text" id="url" name="url" value="<?= is_null($record) ? '' : $record->get('url'); ?>" class="form-control" minlength="2" placeholder="site web..." />
-                        </div>
-                    </div>
+                        <label for="commentaire">Commentaire</label>
+                        <textarea name="commentaire" id="commentaire" class="form-control" rows="10" placeholder="domaine(s) d'activité, remarque et/ou demande que vous auriez concernant la fiche de votre organisation sur notre site..."></textarea>
 
-                    <div class="row mb-3">
-                        <label for="commentaire" class="col-xl-3 col-form-label">Commentaire
-                        </label>
-                        <div class="col-xl-9">
-                            <textarea name="commentaire" id="commentaire" class="form-control" rows="10" placeholder="domaine(s) d'activité, remarque et/ou demande que vous auriez concernant la fiche de votre organisation sur notre site..."></textarea>
-                        </div>
-                    </div>
+                    </fieldset>
 
-                    <div class="row">
-                        <p class="mb-0">
-                            <small>Les champs marqués
-                                <span style="color:#eb0101; font-size: 30px;"><sub>*</sub></span> sont
-                                obligatoires</small>
-                        </p>
-                    </div>
+                    <p class="mb-0">
+                        <small>Les champs marqués
+                            <span style="color:#eb0101; font-size: 30px;"><sub>*</sub></span> sont
+                            obligatoires</small>
+                    </p>
                 </div>
                 <div class="modal-footer">
                     <input class="btn btn-primary" type="submit" value="Envoyer">
--- app/Views/Open/Professional/modal_alter.php
+++ app/Views/Open/Professional/modal_alter.php
@@ -4,16 +4,15 @@ $title = empty($record) ? 'Nouvelle fiche professionelle' : 'Modifier ' . $recor
 ?>
 
 <!-- Modal -->
-<form action="<?= $controller->router()->hyp('submission_submit') ?>" method="POST" role="form">
-
+<form action="<?= $controller->router()->hyp('submission_submit') ?>" method="POST" role="form" class="submission-form">
     <div class="modal fade" id="<?= $modal_reference ?>" tabindex="-1" aria-labelledby="modal-nouvelle-fiche-professionnel-label" aria-hidden="true">
-        <div class="modal-dialog modal-xl modal-dialog-scrollable">
+        <div class="modal-dialog modal-lg modal-dialog-scrollable">
             <div class="modal-content shadow-lg">
                 <div class="modal-header">
                     <h1 class="modal-title p-3 fs-3" id="modal-nouvelle-fiche-professionnel-label"><?= $title ?></h1>
                     <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <div class="modal-body p-5">
+                <div class="modal-body p-3">
                     <p>
                         &rarr; Veuillez compléter les données
                         <br>
@@ -22,109 +21,53 @@ $title = empty($record) ? 'Nouvelle fiche professionelle' : 'Modifier ' . $recor
                         <strong>Cinergie vous remercie de votre collaboration!</strong>
                     </p>
                     <hr>
-                    <section class="row mb-3">
-                        <label for="nom-fiche-professionnel" class="col-lg-3 col-form-label">Nom <span>*</span></label>
-                        <div class="col-lg-9">
-                            <input type="text" id="nom" name="nom-fiche-professionnel" value="" class="form-control" minlength="2" placeholder="nom officiel de votre organisation" required="">
-                        </div>
-                    </section>
+                    <fieldset>
+                        <label for="nom-fiche-professionnel">Nom</label>
+                        <input type="text" id="nom" name="nom-fiche-professionnel" value="" class="form-control" minlength="2" placeholder="nom officiel de votre organisation" required="">
 
-                    <section class="row mb-3">
-                        <label for="date-naiss-fiche-professionnel" class="col-lg-2 col-form-label">Date de naissance
-                            <span>*</span></label>
-                        <div class="col-lg-10">
-                            <input type="date" id="date-naiss" name="date-naiss-fiche-professionnel" value="" class="form-control" required="">
-                        </div>
-                    </section>
+                        <label for="date-naiss-fiche-professionnel">Date de naissance</label>
+                        <input type="date" id="date-naiss" name="date-naiss-fiche-professionnel" value="" class="form-control" required="">
 
-                    <section class="row mb-3">
-                        <label for="adresse-fiche-professionnel" class="col-lg-2 col-form-label">Adresse <span>*</span></label>
-                        <div class="col-lg-10">
-                            <input type="text" id="adresse" name="adresse-fiche-professionnel" value="" class="form-control" minlength="2" placeholder="rue, numéro, boîte" required="">
-                        </div>
-                    </section>
+                        <label for="adresse-fiche-professionnel">Adresse</label>
+                        <input type="text" id="adresse" name="adresse-fiche-professionnel" value="" class="form-control" minlength="2" placeholder="rue, numéro, boîte" required="">
 
-                    <section class="row mb-3">
-                        <label for="cp-fiche-professionnel" class="col-lg-2 col-form-label">Code postal <span>*</span></label>
-                        <div class="col-lg-10">
-                            <input type="text" id="cp" name="cp-fiche-professionnel" value="" class="form-control" minlength="2" placeholder="code postal..." required="">
-                        </div>
-                    </section>
+                        <label for="cp-fiche-professionnel">Code postal</label>
+                        <input type="text" id="cp" name="cp-fiche-professionnel" value="" class="form-control" minlength="2" placeholder="code postal..." required="">
 
-                    <section class="row mb-3">
-                        <label for="ville-fiche-professionnel" class="col-lg-2 col-form-label">Ville <span>*</span></label>
-                        <div class="col-lg-10">
-                            <input type="text" id="ville" name="ville-fiche-professionnel" value="" class="form-control" minlength="2" placeholder="localité" required="">
-                        </div>
-                    </section>
+                        <label for="ville-fiche-professionnel">Ville</label>
+                        <input type="text" id="ville" name="ville-fiche-professionnel" value="" class="form-control" minlength="2" placeholder="localité" required="">
 
-                    <section class="row mb-3">
-                        <label for="province-fiche-professionnel" class="col-lg-2 col-form-label">Province
-                            <span>*</span></label>
-                        <div class="col-lg-10">
-                            <input type="text" id="province" name="province-fiche-professionnel" value="" class="form-control" minlength="2" placeholder="province..." required="">
-                        </div>
-                    </section>
+                        <label for="province-fiche-professionnel">Province</label>
+                        <input type="text" id="province" name="province-fiche-professionnel" value="" class="form-control" minlength="2" placeholder="province..." required="">
 
-                    <section class="row mb-3">
-                        <label for="pays-fiche-professionnel" class="col-lg-2 col-form-label">Pays <span>*</span></label>
-                        <div class="col-lg-10">
-                            <input type="text" id="pays" name="pays-fiche-professionnel" value="" class="form-control" minlength="2" placeholder="pays..." required="">
-                        </div>
-                    </section>
+                        <label for="pays-fiche-professionnel">Pays</label>
+                        <input type="text" id="pays" name="pays-fiche-professionnel" value="" class="form-control" minlength="2" placeholder="pays..." required="">
 
-                    <section class="row mb-3">
-                        <label for="telephone-fiche-professionnel" class="col-lg-2 col-form-label">Téléphone
-                            <span>*</span></label>
-                        <div class="col-lg-10">
-                            <input type="text" id="telephone" name="telephone-fiche-professionnel" value="" class="form-control" minlength="2" placeholder="téléphone" required="">
-                        </div>
-                    </section>
+                        <label for="telephone-fiche-professionnel">Téléphone</label>
+                        <input type="text" id="telephone" name="telephone-fiche-professionnel" value="" class="form-control" minlength="2" placeholder="téléphone" required="">
 
-                    <section class="row mb-3">
-                        <label for="fax-fiche-professionnel" class="col-lg-2 col-form-label">Fax <span>*</span></label>
-                        <div class="col-lg-10">
-                            <input type="text" id="fax" name="fax-fiche-professionnel" value="" class="form-control" minlength="2" placeholder="fax..." required="">
-                        </div>
-                    </section>
+                        <label for="fax-fiche-professionnel">Fax</label>
+                        <input type="text" id="fax" name="fax-fiche-professionnel" value="" class="form-control" minlength="2" placeholder="fax..." required="">
 
-                    <section class="row mb-3">
-                        <label for="gsm-fiche-professionnel" class="col-lg-2 col-form-label">Gsm <span>*</span></label>
-                        <div class="col-lg-10">
-                            <input type="text" id="gsm" name="gsm-fiche-professionnel" value="" class="form-control" minlength="2" placeholder="numéro de gsm" required="">
-                        </div>
-                    </section>
+                        <label for="gsm-fiche-professionnel">Gsm</label>
+                        <input type="text" id="gsm" name="gsm-fiche-professionnel" value="" class="form-control" minlength="2" placeholder="numéro de gsm" required="">
 
-                    <section class="row mb-3">
-                        <label for="email-fiche-professionnel" class="col-lg-2 col-form-label">E-mail <span>*</span></label>
-                        <div class="col-lg-10">
-                            <input type="email" id="email" name="email-fiche-professionnel" value="" class="form-control" minlength="2" placeholder="adresse e-mail..." required="">
-                        </div>
-                    </section>
+                        <label for="email-fiche-professionnel">E-mail</label>
+                        <input type="email" id="email" name="email-fiche-professionnel" value="" class="form-control" minlength="2" placeholder="adresse e-mail..." required="">
 
-                    <section class="row mb-3">
-                        <label for="site-fiche-professionnel" class="col-lg-2 col-form-label">Site <span>*</span></label>
-                        <div class="col-lg-10">
-                            <input type="text" id="site" name="site-fiche-professionnel" value="" class="form-control" minlength="2" placeholder="site web..." required="">
-                        </div>
-                    </section>
+                        <label for="site-fiche-professionnel">Site</label>
+                        <input type="text" id="site" name="site-fiche-professionnel" value="" class="form-control" minlength="2" placeholder="site web..." required="">
+                        
+                        <label for="commentaire-fiche-professionnel">Commentaire</label>
+                        <textarea name="commentaire-fiche-professionnel" id="commentaire" class="form-control" rows="10" placeholder="détaillez vos métiers ici! indiquez une remarque et/ou demande que vous auriez concernant votre fiche professionnelle sur notre site..."></textarea>
 
-                    <section class="row mb-3">
-                        <label for="commentaire-fiche-professionnel" class="col-lg-2 col-form-label">Commentaire
-                            <span>*</span></label>
-                        <div class="col-lg-10">
-                            <textarea name="commentaire-fiche-professionnel" id="commentaire" class="form-control" rows="10" placeholder="détaillez vos métiers ici! indiquez une remarque et/ou demande que vous auriez concernant votre fiche professionnelle sur notre site..."></textarea>
-                        </div>
-                    </section>
+                    </fieldset>
 
-
-                    <section class="row">
-                        <p class="mb-0">
-                            <small>Les champs marqués
-                                <span style="color:#eb0101; font-size: 30px;"><sub>*</sub></span> sont
-                                obligatoires</small>
-                        </p>
-                    </section>
+                    <p class="mb-0">
+                        <small>Les champs marqués
+                            <span style="color:#eb0101; font-size: 30px;"><sub>*</sub></span> sont
+                            obligatoires</small>
+                    </p>
 
                 </div>
                 <div class="modal-footer">
