b29cb65cc4e6457ac15da22da35a2a803a252ebd
HexMakina <20891495+HexMakina@users.noreply.github.com>
2024-03-25 01:27:38 +0100

    deboostrapification search resuts

--- app/Views/Open/Search/tab/professionals.php
+++ app/Views/Open/Search/tab/professionals.php
@@ -1,8 +1,8 @@
 <?php
-use \HexMakina\Marker\{Marker,Form}; 
-?>
-<section class="row mt-4 mb-2 recherche-page">
 
+use \HexMakina\Marker\{Marker, Form};
+?>
+<section class="mt-4 mb-2 recherche-page">
     <div id="filtres-checkbox">
         <form action="<?= $controller->router()->hyp('search'); ?> class=" mt-3">
             <?= Form::hidden('tab', 'pro'); ?>
@@ -22,42 +22,46 @@ use \HexMakina\Marker\{Marker,Form};
                 <button href="#" class="btn btn-primary submit-filters mb-2">Affiner la recherche</button>
             </div>
         </form>
-    </section>
+    </div>
+</section>
 
-    <?php
-    if (empty($professionals->records())) {
-        echo Marker::strong($messageNoResults);
-    } 
-    else {
+<?php
+if (empty($professionals->records())) {
+    echo '<section class="search-no-results">';
+    echo Marker::strong($messageNoResults);
+    echo '<section>';
 
-        foreach ($professionals->records() as $record) {
-            ?>
-                <article class="card shadow p-0 listing mb-3 px-lg-0">
-                    <div class="row g-0">
-                        <div class="col-2 d-flex justify-content-center align-items-center">
-                            <img src="<?= $record->profilePicture(); ?>" class="img-fluid" style="height:200px" alt="Photo de <?= $record->fullName(); ?>">
-                        </div>
-                        <div class="col-10">
-                        <a href="<?=$controller->router()->hyp('professional', ['slug' => $record->slug()])?>">
-                                <div class="row card-body">
-                                    <div class="col-12 col-sm-6 col-md-8">
-                                        <h5 class="card-title mb-0"><?= $record->fullName();?></h5>
-                                    </div>
-                                    <div class="col-6 col-md-4">
-                                        <p class="text-right">20 juillet 2023</p>
-                                    </div>
-                                    <div class="details">
-                                        <p class="card-text text-secondary"><small><?= mb_substr(strip_tags($record->get('content')), 0, 400)?>....</small></p>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                </article>
-        <?php
-        }
-        
-        echo $this->insert('Open::_partials/pagination', ['route' => 'search', 'paginator' => $professionals]);
+} else {
+    echo '<section class="search-results">';
+    foreach ($professionals->records() as $record) {
+
+?>
+        <a href="<?= $controller->router()->hyp('professional', ['slug' => $record->slug()]) ?>" class="card-search shadow">
+            <img src="<?= $record->profilePicture(); ?>" alt="Photo de <?= $record->fullName(); ?>">
+            <h5 class="card-heading"><?= $record->fullName(); ?></h5>
+            <span class="card-meta">
+                <?php
+                foreach ($record->praxisIds() as $id) {
+                    printf('<small class="comma text-secondary otto-id-label" kx-gender="%s" otto-urn="Tag:%d">Tag:%d</small>', $record->get('gender'), $id, $id);
+                }
+                ?>
+            </span>
+            <small class="card-description text-secondary">
+                <?php
+                $content = strip_tags($record->get('content'));
+                if (mb_strlen($content) > 400) {
+                    $content = mb_substr($content, 0, 400) . '...';
+                }
+                echo $content;
+                ?>
+            </small>
+        </a>
+
+<?php
     }
-    ?>
+    echo '</section>';
+
+    echo $this->insert('Open::_partials/pagination', ['route' => 'search', 'paginator' => $professionals]);
+}
+?>
 </section>
