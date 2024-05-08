7f900bc2897bc2d9567d9479294f35ea13a30c37
HexMakina <20891495+HexMakina@users.noreply.github.com>
2024-05-09 00:05:39 +0200

    debootstrap movie card

--- app/Views/Open/Movie/card.php
+++ app/Views/Open/Movie/card.php
@@ -2,18 +2,16 @@
 $url = $controller->router()->hyp('movie', ['slug' => $record->slug()]);
 
 ?>
-        <a class="card-complex" href="<?= $url ?>">
-            <img src="<?= $record->profilePicture() ?>" class="card-img-top" alt="Couverture du film <?= $record->get('label'); ?>">
-        
-            <small class="date text-secondary"><?= $record->get('released'); ?></small>
-            <small class="categorie otto-id-label" otto-urn="Tag:<?= $record->get('genre_id'); ?>"><?= $record->get('genre_id'); ?></small>
+<a class="card-movie  shadow" href="<?= $url ?>">
+    <img src="<?= $record->profilePicture() ?>" class="card-img-top" alt="Couverture du film <?= $record->get('label'); ?>">
 
-            <h5 class="card-title"><?= $record->get('label'); ?></h5>
-            <?php
-            // if (!empty($record->get('directors'))) {
-            //     echo $this->DOM()::span('de ' . $record->get('directors'), ['class' => 'd-block text-end']);
-            // }
-            ?>
-        </a>
-    <!-- </div>
-</article> -->
+    <small class="date text-secondary"><?= $record->get('released'); ?></small>
+    <small class="categorie otto-id-label" otto-urn="Tag:<?= $record->get('genre_id'); ?>"><?= $record->get('genre_id'); ?></small>
+
+    <h5 class="card-title"><?= $record->get('label'); ?></h5>
+    <?php
+    // if (!empty($record->get('directors'))) {
+    //     echo $this->DOM()::span('de ' . $record->get('directors'), ['class' => 'd-block text-end']);
+    // }
+    ?>
+</a>
