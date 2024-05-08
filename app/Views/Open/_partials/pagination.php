1b9b8389e76adea5dda0d709e675a8c8348c008f
HexMakina <20891495+HexMakina@users.noreply.github.com>
2024-05-08 23:49:22 +0200

    fixed: display record count even when no pages exists

--- app/Views/Open/_partials/pagination.php
+++ app/Views/Open/_partials/pagination.php
@@ -1,42 +1,45 @@
-<?php
-if ($paginator->last() > 1) {
-    $params = $controller->router()->params();
-?>
-    <section class="mt-5" id="pagination">
-        <div class="precedent call-to-action">
-            <a class="cta" href="<?= $controller->router()->hyp($route) . '?' . http_build_query(array_merge($params, ['page' => $paginator->previous()])); ?>">Précédent</a>
-        </div>
+<div class="mt-5">
+    <?php
+    if ($paginator->last() > 1) {
+        $params = $controller->router()->params();
+    ?>
+        <section class="mt-5" id="pagination">
+            <div class="precedent call-to-action">
+                <a class="cta" href="<?= $controller->router()->hyp($route) . '?' . http_build_query(array_merge($params, ['page' => $paginator->previous()])); ?>">Précédent</a>
+            </div>
 
-        <div class="pages-numbers text-center">
-            <p>
-                <?php
-                if ($paginator->hasFirstSpacer()) {
-                ?>
-                    <a class="page-btn" href="<?= $controller->router()->hyp($route) . '?' . http_build_query(array_merge($params, ['page' => 1])); ?>">1</a>
-                    <span class="page-btn">...</span>
-                <?php
+            <div class="pages-numbers text-center">
+                <p>
+                    <?php
+                    if ($paginator->hasFirstSpacer()) {
+                    ?>
+                        <a class="page-btn" href="<?= $controller->router()->hyp($route) . '?' . http_build_query(array_merge($params, ['page' => 1])); ?>">1</a>
+                        <span class="page-btn">...</span>
+                    <?php
+                    }
+
+                    foreach ($paginator->range() as $number) {
+                        $class = $paginator->current() == $number ? 'active' : '';
+                        printf('<a class="page-btn %s" href="%s">%d</a>', $class, $controller->router()->hyp($route) . '?' . http_build_query(array_merge($params, ['page' => $number])), $number);
+                    }
+
+                    if ($paginator->hasLastSpacer()) {
+                    ?>
+                        <span class="page-btn">...</span>
+                        <a class="page-btn" href="<?= $controller->router()->hyp($route) . '?' . http_build_query(array_merge($params, ['page' => $paginator->last()])); ?>"><?= $paginator->last() ?></a>
+                    <?php
+                    }
+                    ?>
+                </p>
+
+            </div>
+            <div class="suivant call-to-action">
+
+                <a class="cta" href="<?= $controller->router()->hyp($route) . '?' . http_build_query(array_merge($params, ['page' => $paginator->next()])); ?>">Suivant</a>
+            </div>
+        </section><?php
                 }
-
-                foreach ($paginator->range() as $number) {
-                    $class = $paginator->current() == $number ? 'active' : '';
-                    printf('<a class="page-btn %s" href="%s">%d</a>', $class, $controller->router()->hyp($route) . '?' . http_build_query(array_merge($params, ['page' => $number])), $number);
-                }
-
-                if ($paginator->hasLastSpacer()) {
-                ?>
-                    <span class="page-btn">...</span>
-                    <a class="page-btn" href="<?= $controller->router()->hyp($route) . '?' . http_build_query(array_merge($params, ['page' => $paginator->last()])); ?>"><?= $paginator->last() ?></a>
-                <?php
-                }
-                ?>
-            </p>
-
-        </div>
-        <div class="suivant call-to-action">
-
-            <a class="cta" href="<?= $controller->router()->hyp($route) . '?' . http_build_query(array_merge($params, ['page' => $paginator->next()])); ?>">Suivant</a>
-        </div>
-    </section>
+                    ?>
     <small class="d-block text-center text-secondary text-small">
         <?php
         list($first, $last, $total) = $paginator->nowShowing();
@@ -44,5 +47,4 @@ if ($paginator->last() > 1) {
         ?>
 
     </small>
-<?php
-}
+</div>
