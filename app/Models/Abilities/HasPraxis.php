45a61338ebb500a9ca6f5085e05a9414dd865c26
HexMakina <20891495+HexMakina@users.noreply.github.com>
2024-05-12 14:30:21 +0200

    explode ids using preg_split

--- app/Models/Abilities/HasPraxis.php
+++ app/Models/Abilities/HasPraxis.php
@@ -19,7 +19,7 @@ trait HasPraxis
     public function praxisIds(): array
     {
         if(!empty($this->get('praxis_ids')))
-            return explode(', ',$this->get('praxis_ids'));
+            return preg_split('/,\s*|\s*,\s*/', $this->get('praxis_ids'));
             
         return [];
     }
