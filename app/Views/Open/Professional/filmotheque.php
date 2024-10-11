243227d83844ecc190922e0459e83ae1c337350b
HexMakina <20891495+HexMakina@users.noreply.github.com>
2024-10-11 14:22:03 +0200

    all sliders have dots, no more arrows

--- app/Views/Open/Professional/filmotheque.php
+++ app/Views/Open/Professional/filmotheque.php
@@ -6,17 +6,17 @@ if (!empty($movies)) {
         <h2 class="pb-0">Filmothèque</h2>
         <hr />
         <div class="filmotheque">
-            <div class="slide arrow single-post-slider">
-            <?php
+            <div class="slide dots single-post-slider">
+                <?php
                 foreach ($movies as $movie) {
                     $href = $controller->router()->hyp('movie', ['slug' => $movie->slug()]);
-            ?>
+                ?>
+                    <article class="card paysage mr-4 mb-4">
+                        <div class="film-item">
+                            <?= $this->insert('Open::Movie/card', ['record' => $movie]) ?>
+                        </div>
+                    </article>
 
-                <div class="col-lg-4 film-item">
-                    
-                <?= $this->insert('Open::Movie/card', ['record' => $movie]) ?>
-
-                </div>
                 <?php
                 }
                 ?>
--- app/Views/Open/_partials/photos.php
+++ app/Views/Open/_partials/photos.php
@@ -1,15 +1,13 @@
 <?php if (count($photos) > 1) { ?>
     <div class="row galerie">
         <h2 class="pb-0 line-left">Galerie photos</h2>
-        <div class="slide arrow single-post-slider">
+        <div class="slide dots single-post-slider">
             <?php
             foreach ($photos as $url) {
             ?>
-                <div class="col-lg-4">
-                    <a href="<?= $url ?>" data-lightbox="roadtrip">
+                    <a class="mx-3" href="<?= $url ?>" data-lightbox="roadtrip">
                         <img class="img-galerie img-fluid w-100" src="<?= $url ?>" alt="">
                     </a>
-                </div>
             <?php
             }
             ?>
