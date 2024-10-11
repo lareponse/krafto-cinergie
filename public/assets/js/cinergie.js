bccdc1338ec14ec10d996140804d11d8b57ddc6c
HexMakina <20891495+HexMakina@users.noreply.github.com>
2024-10-11 15:19:09 +0200

    improved code structure

--- public/assets/js/cinergie.js
+++ public/assets/js/cinergie.js
@@ -15,31 +15,28 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   /* long text expander */
-  const labelFold = "Masquer";
-  const labelExpand = "Lire la suite";
-
   const textContent = document.querySelector(".long-text-content");
-  if (textContent) {
-    const textContentHeight = textContent.offsetHeight;
 
-    if (textContentHeight > 500) {
-      textContent.classList.add("folded");
-      textContent.insertAdjacentHTML(
-        "afterend",
-        '<button class="btn btn-primary toggle-button">' +
-          labelExpand +
-          "</button>"
-      );
+  if (textContent && textContent.offsetHeight > 500) {
+    const labelFold = "Masquer";
+    const labelExpand = "Lire la suite";
 
-      let toggleButton = document.querySelector(".toggle-button");
-      toggleButton.addEventListener("click", function () {
-        textContent.classList.toggle("expanded");
-        if (textContent.classList.contains("expanded")) {
-          toggleButton.textContent = labelFold;
-        } else {
-          toggleButton.textContent = labelExpand;
-        }
-      });
-    }
+    textContent.classList.add("folded");
+    textContent.insertAdjacentHTML(
+      "afterend",
+      '<button class="btn btn-primary toggle-button">' +
+        labelExpand +
+        "</button>"
+    );
+
+    let toggleButton = document.querySelector(".toggle-button");
+    toggleButton.addEventListener("click", function () {
+      textContent.classList.toggle("expanded");
+      if (textContent.classList.contains("expanded")) {
+        toggleButton.textContent = labelFold;
+      } else {
+        toggleButton.textContent = labelExpand;
+      }
+    });
   }
 });
