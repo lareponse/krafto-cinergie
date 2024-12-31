88e7b6de47d956439e10e273c3792da218ca7eb8
HexMakina <20891495+HexMakina@users.noreply.github.com>
2024-12-31 22:24:17 +0100

    refactor header and navigation structure; remove unused navbar and searchbar partials; removed boostrap classes + vanilla css responsive navbar

--- app/Views/Open/_partials/header.php
+++ app/Views/Open/_partials/header.php
@@ -1,19 +1,48 @@
-<header class="navbar navbar-expand-xl py-3" id="header">
-    <div class="container">
+<?php
+use HexMakina\Marker\Marker;
 
-        <a class="navbar-brand" href="<?= $controller->router()->hyp('home') ?>">
-            <img src="/public/assets/img/logo-cinergie.svg" alt="Logo CINERGIE" class="logo">
+$activeSection = $actionSection ?? $controller->activeSection();
+
+$menu = [
+    Marker::a($controller->router()->hyp('articles'), 'Articles', ['class' => ('Article' == $activeSection ? 'active' : null)]),
+    Marker::a($controller->router()->hyp('events'), 'Agenda', ['class' => ('Event ' == $activeSection ? 'active' : '')]),
+    Marker::a($controller->router()->hyp('movies'), 'Filmothèque', ['class' => ('Movie' == $activeSection ? 'active' : '')]),
+    Marker::a($controller->router()->hyp('professionals'), 'Professionnel·le·s', ['class' => ('Professional' == $activeSection ? 'active' : '')]),
+    Marker::a($controller->router()->hyp('organisations'), 'Organisations', ['class' => ('Organisation' == $activeSection ? 'active' : '')]),
+    Marker::a($controller->router()->hyp('jobs'), 'Castings & Jobs', ['class' => ('Job' == $activeSection ? 'active' : '')]),
+    Marker::a($controller->router()->hyp('shop'), 'Boutique', ['class' => ('Shop' == $activeSection ? 'active' : '')]),
+    Marker::a('/podcast', 'Podcast', ['class' => ('Podcast' == $activeSection ? 'active' : '')])
+];
+?>
+<form id="search-bar" class="d-print-none" action="<?= $controller->router()->hyp('search') ?>">
+    <div class="fixed-layout">
+        <input type="search" value="" autocomplete="off" placeholder="Rechercher" name="s" value="<?= $controller->router()->params('s') ?>" class="form-control">
+        <button class="btn btn-primary" type="submit" title="Rechercher">
+            <?= $this->bi('search'); ?>
+        </button>
+    </div>
+</form>
+<header>
+    <div class="fixed-layout">
+        <a href="<?= $controller->router()->hyp('home') ?>" title="CINERGIE">
+            <img src="/public/assets/img/logo-cinergie.svg" alt="Logo de Cinergie.be" class="logo">
         </a>
-        <div class="socials d-block d-xl-none ms-auto">
-            <button type="button" data-bs-toggle="collapse" data-bs-target="#searchBAR" aria-controls="searchBAR" aria-expanded="false" aria-label="Toggle navigation" class="mx-1 btn-search">
+
+        <nav id="primary-navigation" role="navigation">
+            <ul> <?php foreach ($menu as $item) {
+                        echo '<li>' . $item . '</li>';
+                    } ?>
+            </ul>
+        </nav>
+
+        <div class="nav-controls">
+            <button class="toggler" type="button" aria-controls="search-bar" aria-expanded="false" aria-label="Toggle navigation">
                 <?= $this->bi('search'); ?>
             </button>
+
+            <button class="toggler" type="button" aria-controls="primary-navigation" aria-expanded="false" aria-label="Toggle navigation">
+                <?= $this->bi('filter-right'); ?>
+            </button>
         </div>
-
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
-            <?= $this->bi('filter-right'); ?>
-        </button>
-
-        <?= $this->insert('Open::_partials/navbar') ?>
     </div>
 </header>
--- app/Views/Open/layout.php
+++ app/Views/Open/layout.php
@@ -6,10 +6,7 @@
 </head>
 
 <body data-bs-spy="scroll" data-bs-target="#navigation">
-
-    <?php $this->insert('Open::_partials/searchbar') ?>
     <?php $this->insert('Open::_partials/header') ?>
-
     <main id="content" class="<?php isset($pageClass) ? $pageClass : '' ?>">
 
         <?php if (isset($title)) {
@@ -25,7 +22,6 @@
             <a href="#top"><?= $this->bi('arrow-up-circle'); ?></a>
         </nav>
     </main>
-
     <?php $this->insert('Open::_partials/footer') ?>
     <?php $this->insert('Open::_partials/cookie') ?>
     <?php $this->insert('Open::_partials/scripts') ?>
