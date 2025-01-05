8e0437ffb1f193ea7712fb7496b52b4ebd94c575
HexMakina <20891495+HexMakina@users.noreply.github.com>
2025-01-05 15:03:47 +0100

    fix: streamline movie view templates by improving structure and removing unnecessary conditions

--- app/Views/Open/Movie/movies.php
+++ app/Views/Open/Movie/movies.php
@@ -3,20 +3,18 @@
 use \HexMakina\Marker\Marker; ?>
 
 <?php $this->layout('Open::layout', ['title' => $page->label()]) ?>
-<div class="container">
 
-    <div id="filmotheque" class="filmotheque catalog">
-        <?php
-        $this->insert('Open/Movie/sidebar_filters');
+<div id="filmotheque" class="filmotheque catalog">
+    <?php
+    $this->insert('Open/Movie/sidebar_filters');
 
-        if (empty($paginator->records()))
-            echo Marker::strong('Aucun résultat ne correspond à vos critères');
-        else {
-            foreach ($paginator->records() as $record) {
-                $this->insert('Open::Movie/card', ['record' => $record]);
-            }
+    if (empty($paginator->records()))
+        echo Marker::strong('Aucun résultat ne correspond à vos critères');
+    else {
+        foreach ($paginator->records() as $record) {
+            $this->insert('Open::Movie/card', ['record' => $record]);
         }
-        ?>
-    </div>
-    <?= $this->insert('Open::_partials/pagination', ['route' => 'movies', 'paginator' => $paginator]); ?>
+    }
+    ?>
 </div>
+<?= $this->insert('Open::_partials/pagination', ['route' => 'movies', 'paginator' => $paginator]); ?>
--- app/Views/Open/Professional/filmotheque.php
+++ app/Views/Open/Professional/filmotheque.php
@@ -1,28 +1,25 @@
 <?php
-if (!empty($movies)) {
+if (empty($movies))
+    return;
 ?>
+<section class="my-5 filmographie">
+    <h2 class="pb-0">Filmothèque</h2>
+    <hr />
+    <div class="filmotheque">
+        <div class="slide dots single-post-slider">
+            <?php
+            foreach ($movies as $movie) {
+                $href = $controller->router()->hyp('movie', ['slug' => $movie->slug()]);
+            ?>
+                <article class="card paysage me-4 mb-4">
+                    <div class="film-item">
+                        <?= $this->insert('Open::Movie/card', ['record' => $movie]) ?>
+                    </div>
+                </article>
 
-    <div class="my-5 filmographie">
-        <h2 class="pb-0">Filmothèque</h2>
-        <hr />
-        <div class="filmotheque">
-            <div class="slide dots single-post-slider">
-                <?php
-                foreach ($movies as $movie) {
-                    $href = $controller->router()->hyp('movie', ['slug' => $movie->slug()]);
-                ?>
-                    <article class="card paysage mr-4 mb-4">
-                        <div class="film-item">
-                            <?= $this->insert('Open::Movie/card', ['record' => $movie]) ?>
-                        </div>
-                    </article>
-
-                <?php
-                }
-                ?>
-            </div>
+            <?php
+            }
+            ?>
         </div>
     </div>
-<?php
-}
-?>
+</section>
