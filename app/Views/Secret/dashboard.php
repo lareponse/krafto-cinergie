0a1443e9390b164a2aa6137e1f9a32a9e8f2d5b8
HexMakina <20891495+HexMakina@users.noreply.github.com>
2025-01-07 02:57:44 +0100

    added .table-clickable

--- app/Views/Secret/dashboard.php
+++ app/Views/Secret/dashboard.php
@@ -1,47 +1,61 @@
 <!DOCTYPE html>
-<html lang="fr" data-theme="light"  data-sidebar-behaviour="fixed" data-navigation-color="inverted" data-is-fluid="true">
+<html lang="fr" data-theme="light" data-sidebar-behaviour="fixed" data-navigation-color="inverted" data-is-fluid="true">
+
 <head>
   <?php $this->insert('Secret::_partials/head') ?>
 </head>
+
 <body>
-    <?php $this->insert('Secret::_partials/theme_configuration') ?>
-    <?php $this->insert('Secret::_partials/navbar') ?>
-    <main>
-        <?php $this->insert('Secret::_partials/header', ['title' => $title]) ?>
+  <?php $this->insert('Secret::_partials/theme_configuration') ?>
+  <?php $this->insert('Secret::_partials/navbar') ?>
+  <main>
+    <?php $this->insert('Secret::_partials/header', ['title' => $title]) ?>
 
 
-        <div class="container-fluid">
-        <?= $this->insert('Secret::_partials/messages') ?>
+    <div class="container-fluid">
+      <?= $this->insert('Secret::_partials/messages') ?>
 
-        <?= $this->section('content')?>
-        </div>
+      <?= $this->section('content') ?>
+    </div>
 
-        <?php $this->insert('Secret::_partials/footer') ?>
-    </main>
+    <?php $this->insert('Secret::_partials/footer') ?>
+  </main>
 
-    <script type="text/javascript" src="/public/assets/dashly/js/theme.bundle.js"></script>
-    <script type="text/javascript" src="/public/assets/js/cinergie.js"></script>
-    
-    <?= $this->section('scripts')?>
-    <script type="module">
-      
-      import OttoIdLabel from '/public/assets/js/otto/otto-id-label.js';
-      import OttoLink from '/public/assets/js/otto/otto-link.js';
-      import OttoFormatDate from '/public/assets/js/otto/otto-format-date.js';
+  <script type="text/javascript" src="/public/assets/dashly/js/theme.bundle.js"></script>
 
-      document.addEventListener("DOMContentLoaded", () => {
-        
-        const ottoIdLabel = new OttoIdLabel('.otto-id-label');
-        ottoIdLabel.replace();
+  <?= $this->section('scripts') ?>
+  <script type="module">
+    import OttoIdLabel from '/public/assets/js/otto/otto-id-label.js';
+    import OttoLink from '/public/assets/js/otto/otto-link.js';
+    import OttoFormatDate from '/public/assets/js/otto/otto-format-date.js';
 
-        OttoLink.urls('.otto-url');
-        OttoLink.emails('.otto-email');
-        OttoLink.calls('.otto-phone');
+    document.addEventListener("DOMContentLoaded", () => {
 
-        OttoFormatDate.searchAndFormat('.otto-date');
-        
-        <?= $this->section('onLoaded')?>
+      const ottoIdLabel = new OttoIdLabel('.otto-id-label');
+      ottoIdLabel.replace();
+
+      OttoLink.urls('.otto-url');
+      OttoLink.emails('.otto-email');
+      OttoLink.calls('.otto-phone');
+
+      OttoFormatDate.searchAndFormat('.otto-date');
+
+
+      // select all .table-clickable rows and make them clickable using their data-action attribute
+      document.querySelectorAll('.table-clickable [data-action]').forEach(row => {
+        row.addEventListener('click', () => {
+          const action = row.getAttribute('data-action');
+          if (action) {
+            window.location.href = action;
+          }
+        });
       });
-    </script>
+
+      // section onLoaded
+      <?= $this->section('onLoaded') ?>
+      // EOF section onLoaded
+    });
+  </script>
 </body>
+
 </html>
