6168b75cac489e202138113b9f01ab6121d5c994
HexMakina <20891495+HexMakina@users.noreply.github.com>
2025-02-02 22:25:26 +0100

    add filter method to Image model for non-deleted image types

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -3,7 +3,16 @@
 namespace App\Models;
 
 use HexMakina\TightORM\TightModel;
+use HexMakina\BlackBox\Database\SelectInterface;
 
 class Image extends TightModel
 {
+
+    public static function filter($filters = [], $options = []): SelectInterface
+    {
+        $filters['deleted'] = 0;
+        $query = parent::filter($filters);
+        $query->whereLike('mime', 'image/%');
+        return $query;
+    }
 }
