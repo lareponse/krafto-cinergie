b4cb21e259d6ba0fc5320aef26a86090f9f14d33
HexMakina <20891495+HexMakina@users.noreply.github.com>
2025-02-02 23:32:49 +0100

    add profile picture path methods and update avatar handling in Author and Team models

--- app/Views/Open/Page/team.php
+++ app/Views/Open/Page/team.php
@@ -4,100 +4,94 @@ use \HexMakina\Marker\Marker; ?>
 
 <?php $this->layout('Open::layout', ['title' => $page->get('label')]) ?>
 
-<div class="container my-5">
-    <div class="row">
-        <div class="col-12">
+<div class="container my-5" id="page_team">
 
-            <section class="row my-5">
-                <?= Marker::p($page->get('content')) ?>
-            </section>
+    <?= Marker::p($page->get('content'), [], false) ?>
 
-            <section class="row my-5" id="equipe">
-                <h2>L'équipe</h2>
+    <section class="team-grid">
+        <h2>L'équipe</h2>
 
-                <?php
-                foreach ($team['equipe'] as $person) {
-                ?>
-                    <div class="col-lg-3 col-md-6" id="professionnel-item">
-                        <article class="card mb-4 shadow">
-                            <div class="card-body">
-                                <a href="professionnel-single.php">
-                                    <img src="/images/<?= $controller->avatarFor($person) ?>" class="card-img-top mb-3" alt="Photo de <?= $person->fullName() ?>">
-                                </a>
-                                <div class="p-3">
-                                    <h5 class="card-title"><?= $person->fullName() ?></h5>
-                                    <p class="card-text"><small class="text-secondary"><?= $person->get('title') ?></small></p>
-                                    <p><a href="mailto:dimitra.bouras@cinergie.be"><?= $person->get('email') ?></a></p>
-                                </div>
-                            </div>
-                        </article>
-                    </div>
-                <?php
-                }
-                ?>
-            </section>
+        <?php
+        foreach ($team['equipe'] as $person) {
+        ?>
+            <article class="shadow">
+                <img src="<?= $controller->avatarFor($person) ?>" class="card-img-top mb-3" alt="Photo de <?= $person->fullName() ?>">
+                <div>
+                    <h3><?= $person->fullName() ?></h3>
+                    <div><small class="text-secondary"><?= $person->get('title') ?></small></div>
+                    <a href="mailto:dimitra.bouras@cinergie.be"><?= $person->get('email') ?></a>
+                </div>
+            </article>
+        <?php
+        }
+        ?>
+    </section>
 
-            <section class="row my-5" id="collabo">
-                <h2>Collaborateur&middot;trice&middot;s régulier&middot;ère&middot;s</h2>
+    <section class="team-grid">
+        <h2>Collaborateur&middot;trice&middot;s régulier&middot;ère&middot;s</h2>
 
-                <?php
-                foreach ($team['collaborateur'] as $person) {
-                ?>
-                    <div class="col-lg-3 col-md-6" id="professionnel-item">
-                        <article class="card mb-4 shadow">
-                            <div class="card-body">
-                                <a href="<?= $controller->router()->hyp('author', ['slug' => $person->slug()]) ?>">
-                                    <img src="<?= $controller->avatarFor($person) ?>" class="card-img-top mb-3" alt="...">
-                                </a>
-                                <div class="p-3">
-                                    <h5 class="card-title"><?= $person->fullName(); ?></h5>
-                                </div>
-                            </div>
-                        </article>
-                    </div>
-                <?php
-                }
-                ?>
+        <?php
+        foreach ($team['collaborateur'] as $person) {
+        ?>
+            <article class="shadow">
+                <a href="<?= $controller->router()->hyp('author', ['slug' => $person->slug()]) ?>">
+                    <img src="<?= $controller->avatarFor($person) ?>" class="card-img-top mb-3" alt="">
+                </a>
+                <div>
+                    <h5><?= $person->fullName(); ?></h5>
+                </div>
+            </article>
+        <?php
+        }
+        ?>
+    </section>
 
-            </section>
-
-            <section class="row my-5">
-                <?php
-
-                if (isset($team['CA'])) {
-                    $content = [];
-                    foreach ($team['CA'] as $person) {
-                        $content[] = $person->get('title') . ' : ' . $person->fullName();
-                    }
-
-                    echo Marker::h3("Conseil d'administration");
-                    echo Marker::p(implode(Marker::br(), $content));
+    <section class="d-flex justify-content-between flex-wrap">
+        <div>
+            <?php
+            if (isset($team['CA'])) {
+                $content = [];
+                foreach ($team['CA'] as $person) {
+                    $content[] = $person->get('title') . ' : ' . $person->fullName();
                 }
 
-                if (isset($team['membre'])) {
+                echo Marker::h3("Conseil d'administration");
+                echo Marker::p(implode(Marker::br(), $content), [], false);
+            }
+            ?>
 
-                    $content = [];
-                    foreach ($team['membre'] as $person) {
-                        $content[] = $person->fullName();
-                    }
-
-                    echo Marker::h3('Membres');
-                    echo Marker::p(implode(Marker::br(), $content));
-                }
-
-                if (isset($team['observateur'])) {
-
-                    $content = [];
-
-                    foreach ($team['observateur'] as $person) {
-                        $content[] = Marker::strong($person->get('title')) . Marker::br() . $person->fullName();
-                    }
-                    echo Marker::h3('Observateur·trice·s');
-                    echo Marker::p(implode(Marker::br(), $content));
-                }
-                ?>
-            </section>
-        
         </div>
-    </div>
+        <div>
+            <?php
+
+            if (isset($team['membre'])) {
+
+                $content = [];
+                foreach ($team['membre'] as $person) {
+                    $content[] = $person->fullName();
+                }
+
+                echo Marker::h3('Membres');
+                echo Marker::p(implode(Marker::br(), $content), [], false);
+            }
+            ?>
+
+        </div>
+        <div>
+            <?php
+
+            if (isset($team['observateur'])) {
+
+                $content = [];
+
+                foreach ($team['observateur'] as $person) {
+                    $content[] = Marker::strong($person->get('title')) . Marker::br() . $person->fullName();
+                }
+                echo Marker::h3('Observateur·trice·s');
+                echo Marker::p(implode(Marker::br(), $content), [], false);
+            }
+            ?>
+        </div>
+    </section>
+
 </div>
