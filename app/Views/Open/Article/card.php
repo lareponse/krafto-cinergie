c5e7e63bf9933064879d571a34a10aae8e02790e
HexMakina <20891495+HexMakina@users.noreply.github.com>
2025-02-03 19:23:33 +0100

    fix: update avatar image source to use thumbnail size

--- app/Views/Open/Article/card.php
+++ app/Views/Open/Article/card.php
@@ -1,6 +1,6 @@
 <article class="article-item card mb-4 shadow ">
     <a href="<?= $controller->router()->hyp('article', ['slug' => $article->slug()]); ?>">
-        <img src="<?= $controller->defaultAvatar();?>" loading="lazy" data-src="<?= $controller->avatarFor($article) ?>" class="card-img-top" alt="<?= $article->get('label'); ?> - <?= $article->get('author_label'); ?>"" />
+        <img src="<?= $controller->defaultAvatar();?>" loading="lazy" data-src="<?= $controller->avatarFor($article, 'thumbnail') ?>" class="card-img-top" alt="<?= $article->get('label'); ?> - <?= $article->get('author_label'); ?>"" />
         <div class=" card-body">
         <?php if (!isset($showCategory) || $showCategory === true) { ?>
             <p class="btn btn-sm btn-primary taxo-cat otto-id-label" otto-urn="Tag:<?= $article->get('type_id'); ?>">Tag:<?= $article->get('type_id'); ?></p>
