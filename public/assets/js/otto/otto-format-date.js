6758e1498a488b2ec549f7ea4f1fae38474875ce
HexMakina <touch@hexmakina.be>
2025-02-23 16:45:44 +0100

    feature: otto-date-relative

--- public/assets/js/otto/otto-format-date.js
+++ public/assets/js/otto/otto-format-date.js
@@ -1,6 +1,6 @@
 class OttoFormatDate {
 
-    static getFormat(container){
+    static getFormat(container) {
         if (container.getAttribute('otto-format')) {
             try {
                 const format = decodeURIComponent(container.getAttribute('otto-format'))
@@ -11,27 +11,56 @@ class OttoFormatDate {
                 console.log('Container:', container)
                 console.log('parsedFormat:', parsedFormat)
             }
-            
+
         }
         return { dateStyle: "long" }
     }
 
-    static searchAndFormat(selector = '.otto-date'){
+    static searchAndFormat(selector = '.otto-date') {
         let dateText, date, dateFormat
 
         document.querySelectorAll(selector).forEach(container => {
             dateText = container.innerText.trim();
 
             date = new Date(dateText)
-            
+
             if (isNaN(date)) {
                 console.error('Invalid dateText', container.innerText)
             }
-            else{
-                dateFormat = new Intl.DateTimeFormat('fr-FR', OttoFormatDate.getFormat(container))
-                container.innerText = dateFormat.format(date)
+            else {
+                if(container.classList.contains('otto-date-relative')){
+                    container.innerText = OttoFormatDate.timeAgo(date)
+                }
+                else{
+                    dateFormat = new Intl.DateTimeFormat('fr-FR', OttoFormatDate.getFormat(container))
+                    container.innerText = dateFormat.format(date)
+                }
+
             }
         });
+    }
+
+    static timeAgo(date) {
+        const now = new Date();
+        const diffMs = date - now;
+        const units = [
+            { unit: 'year', divisor: 31536000000 },
+            { unit: 'month', divisor: 2592000000 },
+            { unit: 'week', divisor: 604800000 },
+            { unit: 'day', divisor: 86400000 },
+            { unit: 'hour', divisor: 3600000 },
+            { unit: 'minute', divisor: 60000 },
+            { unit: 'second', divisor: 1000 }
+        ];
+
+        const rtf = new Intl.RelativeTimeFormat('fr', { numeric: 'auto' });
+
+        for (const { unit, divisor } of units) {
+            const value = Math.round(diffMs / divisor);
+            if (Math.abs(value) >= 1 || unit === 'second') {
+                return rtf.format(value, unit);
+            }
+        }
     }
 }
 
