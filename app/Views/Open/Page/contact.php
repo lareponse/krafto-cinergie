8dc7b54df7a3c367639c0b80d6e8eab01214e656
HexMakina <20891495+HexMakina@users.noreply.github.com>
2025-04-17 16:55:14 +0200

    replace Google Maps iframe with a link and add custom map background image

--- app/Views/Open/Page/contact.php
+++ app/Views/Open/Page/contact.php
@@ -37,10 +37,9 @@ $this->layout('Open::layout', ['title' => $page->get('label')]);
             </div>
 
             <div class="col-lg-6 px-0">
-                <div class="map-wrap">
-                    <iframe id="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2519.1815057838608!2d4.368381215736397!3d50.84632307953197!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c3c482b3ed252b%3A0x55f1f2ac34267037!2sMaison%20de%20la%20Francit%C3%A9%20ASBL!5e0!3m2!1sfr!2sbe!4v1676372527065!5m2!1sfr!2sbe" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                </div>
+                <a href="https://www.google.com/maps/place/Cinergie/@50.8471385,4.3700846,685m/data=!3m1!1e3!4m6!3m5!1s0x47c3c46473edbda1:0x7b4e6935f3f34fb8!8m2!3d50.8468541!4d4.3699327!16s%2Fg%2F1vd70h48?entry=ttu&g_ep=EgoyMDI1MDQxNC4xIKXMDSoASAFQAw%3D%3D" class="map-wrap"></a>
             </div>
         </div>
     </div>
+</div>
 </div>
