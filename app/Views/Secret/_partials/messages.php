977e26147bce18450b03fe39ec2b7442b8b6e757
HexMakina <20891495+HexMakina@users.noreply.github.com>
2025-05-18 23:58:17 +0200

    update user message alerts to display localized success, error, warning, and info messages

--- app/Views/Secret/_partials/messages.php
+++ app/Views/Secret/_partials/messages.php
@@ -2,9 +2,21 @@
 if (!empty($user_messages)) {
     foreach ($user_messages as $level => $messages) {
         $class = $level == 'notice' ? 'success' : $level;
-        foreach ($messages as $messageWithContext) {
-            list($message, $context) = $messageWithContext;
-            echo sprintf('<div class="alert alert-%s" role="alert">%s</div>', $class, $messageWithContext[0]);
+        if($class == 'success') {
+            echo sprintf('<div class="alert alert-%s" role="alert">%s</div>', $class, 'Toutes les modifications ont été enregistrées avec succès');
         }
+        elseif($class == 'error') {
+            echo sprintf('<div class="alert alert-%s" role="alert">%s</div>', $class, 'Une erreur est survenue lors de l\'enregistrement des modifications');
+        }
+        elseif($class == 'warning') {
+            echo sprintf('<div class="alert alert-%s" role="alert">%s</div>', $class, 'Attention : certaines modifications n\'ont pas été enregistrées');
+        }
+        elseif($class == 'info') {
+            echo sprintf('<div class="alert alert-%s" role="alert">%s</div>', $class, 'Information : certaines modifications ont été enregistrées avec succès');
+        }
+        // foreach ($messages as $messageWithContext) {
+        //     list($message, $context) = $messageWithContext;
+        //     echo sprintf('<div class="alert alert-%s" role="alert">%s</div>', $class, $messageWithContext[0]);
+        // }
     }
 }
