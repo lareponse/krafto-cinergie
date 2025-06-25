9b39dfe577f6a240d3261c3072e73c941c867b01
HexMakina <20891495+HexMakina@users.noreply.github.com>
2025-06-25 22:27:34 +0200

    refactor foreign key constraints in movie_theme table for improved integrity

--- import/003/006_tag_categories_cleanup.sql
+++ import/003/006_tag_categories_cleanup.sql
@@ -1,3 +1,20 @@
+ALTER TABLE `movie_theme` 
+DROP FOREIGN KEY `movie_theme-hasMovie`; 
+
+ALTER TABLE `movie_theme` 
+ADD CONSTRAINT `movie_theme-hasMovie` 
+FOREIGN KEY (`movie_id`) 
+REFERENCES `movie`(`id`) 
+ON DELETE CASCADE ON UPDATE RESTRICT; 
+
+ALTER TABLE `movie_theme` DROP FOREIGN KEY `movie_theme-hasTag`;
+ALTER TABLE `movie_theme` 
+ADD CONSTRAINT `movie_theme-hasTag` 
+FOREIGN KEY (`tag_id`) 
+REFERENCES `tag`(`id`) 
+ON DELETE CASCADE ON UPDATE RESTRICT;
+
+
 -- Shows all rows with parent_id = 1, indicating if they'll be kept or deleted
 SELECT id, 
        label,
