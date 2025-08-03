1b7d50c835c68ad6e11c7358c4b1653402005ffc
HexMakina <20891495+HexMakina@users.noreply.github.com>
2025-08-03 16:53:12 +0200

    refactor message display logic to improve error handling and alert classification

--- app/Views/Secret/_partials/messages.php
+++ app/Views/Secret/_partials/messages.php
@@ -1,22 +1,31 @@
 <?php
 if (!empty($user_messages)) {
     foreach ($user_messages as $level => $messages) {
-        $class = $level == 'notice' ? 'success' : $level;
-        if($class == 'success') {
+
+
+        $class = $level;
+        if ($level === 'notice')
+            $class = 'success';
+        elseif ($level === 'error')
+            $class = 'danger';
+
+        if ($class == 'success') {
             echo sprintf('<div class="alert alert-%s" role="alert">%s</div>', $class, 'Toutes les modifications ont été enregistrées avec succès');
-        }
-        elseif($class == 'error') {
-            echo sprintf('<div class="alert alert-%s" role="alert">%s</div>', $class, 'Une erreur est survenue lors de l\'enregistrement des modifications');
-        }
-        elseif($class == 'warning') {
+        } elseif ($level == 'error') {
+
+            $message = 'Une erreur est survenue lors de l\'enregistrement des modifications';
+            if (is_array($messages) && count($messages) > 1)
+                $message = 'Plusieurs erreurs sont survenues lors de l\'enregistrement des modifications';
+
+            foreach ($messages as $m)
+                $message .= '<br>' . (is_array($m) ? $m[0] : $m);
+
+            echo sprintf('<div class="alert alert-%s" role="alert">%s</div>', $class, $message);
+        } elseif ($class == 'warning') {
+            vd($messages);
             echo sprintf('<div class="alert alert-%s" role="alert">%s</div>', $class, 'Attention : certaines modifications n\'ont pas été enregistrées');
-        }
-        elseif($class == 'info') {
+        } elseif ($class == 'info') {
             echo sprintf('<div class="alert alert-%s" role="alert">%s</div>', $class, 'Information : certaines modifications ont été enregistrées avec succès');
         }
-        // foreach ($messages as $messageWithContext) {
-        //     list($message, $context) = $messageWithContext;
-        //     echo sprintf('<div class="alert alert-%s" role="alert">%s</div>', $class, $messageWithContext[0]);
-        // }
     }
 }
